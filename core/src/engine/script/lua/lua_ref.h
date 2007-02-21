302de8aaae6e62dd94f60dd708d33c832e7d5034
phoku <klaus.blindert@web.de>
2007-02-21 19:04:13 +0000

    This commit was brought to you by the documentation task force :-)

--- core/src/engine/script/lua/lua_ref.h
+++ core/src/engine/script/lua/lua_ref.h
@@ -31,21 +31,55 @@
 
 namespace FIFE {
 
+	/** Utility class for objects that hold refernces to lua objects.
+	 * @bug What happens if the state is destroyed and a reference is still hold?
+	 */
 	class LuaRef {
 		public:
+			/** Constructor
+			 *  @note Initially @see isValid should return @c false
+			 */
 			LuaRef();
+
+			/** Destructor
+			 *  @note This will call unref. Just in case.
+			 */
 			~LuaRef();
 
+			/** Reference a object on the lua stack
+			 *  Grabs a refrence of an object on the lua stack,
+			 *  and also saves the lua state for later reference.
+			 *  @note This will call unref. Just in case.
+			 *  @note After this call @see isValid should return @c true.
+			 *  @param index Index of the element on the stack, that should be referenced.
+			 *  @param L the current lua state.
+			 */
 			void ref(lua_State *L, int index);
+
+			/** Release any reference held
+			 *  @note After this call @see isValid will return @c false
+			 */
 			void unref();
 
+			/** Check whether currentl a reference is held.
+			 */
 			bool isValid() const;
 
+			/** Push the referenced object on the lua stack it was referenced in.
+			 *  The refrenced object will be on the top of the current stack,
+			 *  @note Do @b not call this if @c isValid returns @c false
+			 */
 			void push();
+
+			/** Return the lua stack a lua object was referenced in.
+			 *  @note Do @b not call this if @c isValid returns @c false
+			 */
 			lua_State* getState();
 
 		protected:
+			// The lua ref as returned by luaL_ref
 			int m_ref;
+			// The lua state the reference was opetained in
 			lua_State* m_state;
 	};
 
