08f0a60da90f93f7e8a82414bd75cdf0b7a37791
phoku <klaus.blindert@web.de>
2007-04-22 09:37:57 +0000

    More robust test running. Cleaner code.

--- core/src/unit_tests/test_all.py
+++ core/src/unit_tests/test_all.py
@@ -6,18 +6,25 @@ if len(sys.argv) > 1:
     print '  Runs all test cases defined in SConscript file of unit_tests-folder\n'
     sys.exit(0)
 
+# Switch to unit_tests directory
 dirpath = os.sep.join(sys.argv[0].split(os.sep)[:-1])
-reprg = re.compile(r"""^env.Program\(["'](.*?)['"]""")
-progs = []
-for line in open(os.sep.join((dirpath, 'SConscript'))):
-    m = reprg.match(line.strip())
-    if m:
-        progs.append(m.group(1))
+if dirpath:
+    os.chdir(dirpath)
+
+def getTestPrograms():
+    """ Get the names of all test programs by evaluating the SConscript file """
+    reprg = re.compile(r"""^env.Program\(["'](.*?)['"]""")
+    progs = []
+    for line in open("SConscript"):
+        m = reprg.match(line.strip())
+        if m:
+            progs.append(m.group(1))
+    return progs
 
 failed = []
-for prog in progs:
+for prog in getTestPrograms():
     print '\n===== Running %s =====' % prog
-    if os.system(os.sep.join(('.', dirpath, prog))):
+    if os.system(os.sep.join(('.', prog))):
         failed.append( prog )
 
 if failed:
