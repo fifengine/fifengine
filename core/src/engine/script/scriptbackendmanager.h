ce472b0e950805171a417435fe0197892d67c887
jasoka <j.jasoka@gmail.com>
2007-04-26 22:50:52 +0000

    backend manager pushed to util

--- core/src/engine/script/scriptbackendmanager.h
+++ core/src/engine/script/scriptbackendmanager.h
@@ -30,7 +30,7 @@
 // These includes are split up in two parts, separated by one empty line
 // First block: files included from the FIFE root src directory
 // Second block: files included from the same folder
-#include "backendmanager.h"
+#include "util/backendmanager.h"
 #include "util/singleton.h"
 
 #include "scriptengine.h"
