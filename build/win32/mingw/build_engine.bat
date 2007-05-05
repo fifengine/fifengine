fa64eb8f753337d50eda76339b91325fe9ba99c9
mvbarracuda <mvbarracuda@web.de>
2007-05-05 16:35:03 +0000

    * Modified win32 build paths to work with the soon to be released new compile SDK PART 2

--- build/win32/mingw/build_engine.bat
+++ build/win32/mingw/build_engine.bat
@@ -1,6 +1,6 @@
 :: User would need to edit to put the path to their Scons installation
 set _=%CD%
-SET PATH=%Path%;%_%\Scons;%_%\MinGW\bin
+SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
 
 :: User would need to edit to point to where they have it stored
 cd \
