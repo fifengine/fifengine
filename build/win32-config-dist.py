b30eeb5ccf676535880668923dabb6a5aff57abe
mvbarracuda <mvbarracuda@web.de>
2007-05-05 16:00:18 +0000

    * Modified win32 build paths to work with the soon to be released new compile SDK  * Added msvc2005 folder

--- build/win32-config-dist.py
+++ build/win32-config-dist.py
@@ -7,11 +7,11 @@ def initEnvironment(env):
 
 def addExtras(context):
 	path = os.environ['_']
-	context.env.Append(CPPPATH = [path + '\\OpenAL\\Include', path + '\\SDL_image', path + '\\zlib', path + '\\lua51\\include', path + '\\guichan', path + '\\boost', path + '\\libvorbis', path, path + '\\sdl_ttf\\include', path + '\\sdl\\include'])
-	context.env.Append(LIBPATH = [path + '\\lib_diverse'])
+	context.env.Append(CPPPATH = [path + '\\include', path + '\\include\\openal\\Include', path + '\\include\\sdl_image', path + '\\include\\zlib', path + '\\include\\\lua51\\include', path + '\\include\\guichan', path + '\\include\\boost', path + '\\include\\libvorbis', path, path + '\\include\\sdl_ttf\\include', path + '\\include\\sdl\\include'])
+	context.env.Append(LIBPATH = [path + '\\static_libs'])
 	
 	context.env.Append(LIBS = ['fife', 'mingw32', 'zlib', 'SDL_image', 'lua51', 'guichan', 'guichan_sdl', 'vorbis', 'ogg', 'vorbisfile', 'SDLmain', 'SDL', 'OpenAL32', 'SDL_ttf', 'boost_filesystem', 'boost_regex'])
 
 	if context.env['opengl']:
-		context.env.Append(CPPPATH = [path + '\\opengl\\include'])
+		context.env.Append(CPPPATH = [path + '\\include\\\opengl\\include'])
 		context.env.Append(LIBS = ['guichan_opengl', 'opengl32', 'glu32'])
