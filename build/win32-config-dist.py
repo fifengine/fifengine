c48d7ae6f563dcb2415bef36a3ebd59b52673ba9
mvbarracuda <mvbarracuda@web.de>
2007-05-06 17:49:29 +0000

    * Doxygen file (still hardcoded paths)  * MSVC project update batch file for win32  * Simplified include paths for mingw users

--- build/win32-config-dist.py
+++ build/win32-config-dist.py
@@ -7,11 +7,11 @@ def initEnvironment(env):
 
 def addExtras(context):
 	path = os.environ['_']
-	context.env.Append(CPPPATH = [path + '\\include', path + '\\include\\openal\\Include', path + '\\include\\sdl_image', path + '\\include\\zlib', path + '\\include\\\lua51\\include', path + '\\include\\guichan', path + '\\include\\boost', path + '\\include\\libvorbis', path, path + '\\include\\sdl_ttf\\include', path + '\\include\\sdl\\include'])
+	context.env.Append(CPPPATH = [path + '\\include', path + '\\include\\openal', path + '\\include\\sdl_image', path + '\\include\\zlib', path + '\\include\\\lua51', path + '\\include\\guichan', path + '\\include\\boost', path + '\\include\\libvorbis', path, path + '\\include\\sdl_ttf', path + '\\include\\sdl'])
 	context.env.Append(LIBPATH = [path + '\\static_libs'])
 	
 	context.env.Append(LIBS = ['fife', 'mingw32', 'zlib', 'SDL_image', 'lua51', 'guichan', 'guichan_sdl', 'vorbis', 'ogg', 'vorbisfile', 'SDLmain', 'SDL', 'OpenAL32', 'SDL_ttf', 'boost_filesystem', 'boost_regex'])
 
 	if context.env['opengl']:
-		context.env.Append(CPPPATH = [path + '\\include\\\opengl\\include'])
+		context.env.Append(CPPPATH = [path + '\\include\\\opengl'])
 		context.env.Append(LIBS = ['guichan_opengl', 'opengl32', 'glu32'])
---
+++ build/win32/msvc2005/update_msvc_project.bat
@@ -0,0 +1,11 @@
+:: Users will need to change paths if they use custom MinGW or SCons version that didn't ship with the SDK
+set _=%CD%
+SET PATH=%Path%;%_%\..\mingw\applications\scons
+
+:: Goto TRUNK and call SCons
+cd \
+cd %_%\..\..\..\
+scons msvcproj=1 >> "%_%\msvc_update_log.txt" 2>&1
+
+:: Return us to the directory we started from
+cd %_%
