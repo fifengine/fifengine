08c4e44d8767f13368fe50df2be60bb52692ef39
vovansim <vovansim@gmail.com>
2007-05-23 20:43:05 +0000

    Filled out all the unit tests except file reading. Now to make them work.

--- tests/unit_tests/test_zip.cpp
+++ tests/unit_tests/test_zip.cpp
@@ -62,7 +62,7 @@ void test_isReadable() {
 	boost::shared_ptr<VFS> pvfs = initialize_vfs(false);
 	FIFE::zip::ZipProvider provider;
 	if (!provider.isReadable(ZIP_FILE)) {
-		BOOST_ERROR("Test zip archive \"" << ZIP_FILE << "\" not readable.");
+		BOOST_FAIL("Test zip archive \"" << ZIP_FILE << "\" not readable.");
 	}
 }
 
@@ -70,7 +70,7 @@ void test_isNotReadable() {
 	boost::shared_ptr<VFS> pvfs = initialize_vfs(false);
 	FIFE::zip::ZipProvider provider;
 	if (provider.isReadable("data/dat1vfstest.dat")) {
-		BOOST_ERROR("False positive on a non-zip file.");
+		BOOST_FAIL("False positive on a non-zip file.");
 	}
 }
 
@@ -78,44 +78,72 @@ void test_createSource() {
 	boost::shared_ptr<VFS> pvfs = initialize_vfs(false);
 	FIFE::zip::ZipProvider provider;
 	if (provider.createSource(ZIP_FILE) == NULL) {
-		BOOST_ERROR("Provider does not make a valid source.");
+		BOOST_FAIL("Provider does not make a valid source.");
 	}
+}
+
+void test_dirExists() {
+	Log::setLogLevel(Log::LEVEL_MAX);
+	boost::shared_ptr<VFS> pvfs = initialize_vfs(true);
+	if (!pvfs->exists("ziptestdir"))
+		BOOST_FAIL("Source reports existing dir as missing.");
+}
+
+void test_dirNotExists() {
+	Log::setLogLevel(Log::LEVEL_MAX);
+	boost::shared_ptr<VFS> pvfs = initialize_vfs(true);
+	if (pvfs->exists("ziptestdir_nonexistant"))
+		BOOST_FAIL("Source reports non-existant dir as present.");
 }
 
 void test_fileExists() {
 	Log::setLogLevel(Log::LEVEL_MAX);
-	initialize_vfs(true);
-	BOOST_ERROR("Blank Test. Please provide an implementation");
+	boost::shared_ptr<VFS> pvfs = initialize_vfs(true);
+	if (!pvfs->exists("ziptestdir/ziptest.map"))
+		BOOST_FAIL("Source reports existing file as missing.");
 }
 
 void test_fileNotExists() {
 	Log::setLogLevel(Log::LEVEL_MAX);
-	initialize_vfs(true);
-	BOOST_ERROR("Blank Test. Please provide an implementation");
+	boost::shared_ptr<VFS> pvfs = initialize_vfs(true);
+	if (pvfs->exists("ziptestdir/ziptest_doesnotexist.map"))
+		BOOST_FAIL("Source reports non-existant file as present.");
 }
 
 void test_listFiles() {
 	Log::setLogLevel(Log::LEVEL_MAX);
-	initialize_vfs(true);
-	BOOST_ERROR("Blank Test. Please provide an implementation");
+	boost::shared_ptr<VFS> pvfs = initialize_vfs(true);
+	VFS::type_stringlist fileList = pvfs->listFiles("ziptestdir");
+	if (fileList.size() != 1)
+		BOOST_FAIL("ziptestdir is supposed to have 1 file. ZipSource says it has " << fileList.size());
+	if (fileList.find("ziptest.map") == fileList.end())
+		BOOST_FAIL("ZipSource does not report ziptest.map in the file list.");
 }
 
 void test_listDirectories() {
 	Log::setLogLevel(Log::LEVEL_MAX);
-	initialize_vfs(true);
-	BOOST_ERROR("Blank Test. Please provide an implementation");
+	boost::shared_ptr<VFS> pvfs = initialize_vfs(true);
+	VFS::type_stringlist dirList = pvfs->listDirectories("ziptestdir");
+	if (dirList.size() != 2)
+		BOOST_FAIL("ziptestdir is supposed to have 2 directories. ZipSource says it has " << dirList.size());
+	if (dirList.find("ziptestsubdir1") == dirList.end())
+		BOOST_FAIL("ZipSource does not report ziptestsubdir1 in the directory list.");
+	if (dirList.find("ziptestsubdir2") == dirList.end())
+		BOOST_FAIL("ZipSource does not report ziptestsubdir2 in the directory list.");
 }
 
 void test_fileRead() {
 	Log::setLogLevel(Log::LEVEL_MAX);
-	initialize_vfs(true);
-	BOOST_ERROR("Blank Test. Please provide an implementation");
+	boost::shared_ptr<VFS> pvfs = initialize_vfs(true);
+	BOOST_FAIL("Blank Test. Please provide an implementation");
 }
 
 test_suite* init_unit_test_suite(int argc, char* argv[]) {
 	test_suite* test = BOOST_TEST_SUITE("VFS Zip tests");
 	test->add(BOOST_TEST_CASE(&test_isReadable), 0);
 	test->add(BOOST_TEST_CASE(&test_isNotReadable), 0);
+	test->add(BOOST_TEST_CASE(&test_dirExists), 0);
+	test->add(BOOST_TEST_CASE(&test_dirNotExists), 0);
 	test->add(BOOST_TEST_CASE(&test_fileExists), 0);
 	test->add(BOOST_TEST_CASE(&test_fileNotExists), 0);
 	test->add(BOOST_TEST_CASE(&test_listFiles), 0);
