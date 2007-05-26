bb3c9e55c6f264ec3e3ec9ebf774f8e82f9da160
mvbarracuda <mvbarracuda@web.de>
2007-05-26 11:47:20 +0000

    * Slightly changed PATH vars to avoid path collisions in case the user is running other versions of mingw and scons on his system too

--- build/win32/mingw/logbuild_engine.bat
+++ build/win32/mingw/logbuild_engine.bat
@@ -1,6 +1,9 @@
 :: Users will need to change paths if they use custom MinGW or SCons version that didn't ship with the SDK
 set _=%CD%
-SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
+
+:: To avoid path collisions the following line was commented out and replaced with a slightly different version
+:: SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
+SET PATH=%_%\applications\scons;%_%\applications\mingw\bin
 
 :: Delete engine_build_log.txt in case that it's still in place from a former compile sessions
 del %_%\engine_build_log.txt
