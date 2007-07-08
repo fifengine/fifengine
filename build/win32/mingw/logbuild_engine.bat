685f5b886b418e0014f35c6b17f3a25913986963
mvbarracuda <mvbarracuda@web.de>
2007-07-08 23:04:29 +0000

    * Modified build files to get the extend branch working on win32  * Just works with code::blocks for now; scons in combination with mingw still broken  * This won't work out of the box! I had to modify some sconstruct and sconscript files and add hardcoded paths to python there, nevertheless we're heading to the right direction

--- build/win32/mingw/logbuild_engine.bat
+++ build/win32/mingw/logbuild_engine.bat
@@ -3,7 +3,7 @@ set _=%CD%
 
 :: To avoid path collisions the following line was commented out and replaced with a slightly different version
 :: SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
-SET PATH=%_%\..\applications\scons;%_%\..\applications\mingw\bin
+SET PATH=%_%\..\applications\scons;%_%\..\applications\mingw\bin;..\applications\swigwin-1.3.31;..\applications\python25
 
 :: Delete engine_build_log.txt in case that it's still in place from a former compile sessions
 del %_%\engine_build_log.txt
@@ -11,7 +11,7 @@ del %_%\engine_build_log.txt
 :: Goto TRUNK and call SCons
 cd \
 cd %_%\..\..\..
-scons debug=0 >> "%_%\engine_build_log.txt" 2>&1
+scons debug=0 zip=1 >> "%_%\engine_build_log.txt" 2>&1
 
 :: Return us to the directory we started from
 cd %_%
