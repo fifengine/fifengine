e4deee8a83cb5a5d6b9799f0b55dc78b66db9deb
mvbarracuda <mvbarracuda@web.de>
2007-08-25 13:17:44 +0000

    * Updated the metamodel branch and trunk to work with the latest compile SDK (released today)  * You'll need to install ActivePython 2.5 to work with FIFE on Win32 now: http://downloads.activestate.com/ActivePython/windows/2.5/ActivePython-2.5.1.1-win32-x86.msi  * Neither trunk nor the metamodel branch will build on win32, but there not much we can do about it without an active programmer who's utilizing the Microsoft compiler

--- build/win32-config-dist.py
+++ build/win32-config-dist.py
@@ -7,8 +7,8 @@ def initEnvironment(env):
 
 def addExtras(context):
 	path = os.environ['_']
-	context.env.Append(CPPPATH = [path, path + '\\include', path + '\\include\\openal', path + '\\include\\sdl_image', path + '\\include\\zlib', path + '\\include\\\lua51', path + '\\include\\guichan', path + '\\include\\boost', path + '\\include\\libvorbis', path + '\\include\\sdl_ttf', path + '\\include\\sdl', path + '\\include\\png', path + '\\include\\gettext', path + '\\..\\applications\\python25\\include'])
-	context.env.Append(LIBPATH = [path + '\\static_libs', path + '\\..\\applications\\python25\\libs'])
+	context.env.Append(CPPPATH = [path, path + '\\include', path + '\\include\\openal', path + '\\include\\sdl_image', path + '\\include\\zlib', path + '\\include\\\lua51', path + '\\include\\guichan', path + '\\include\\boost', path + '\\include\\libvorbis', path + '\\include\\sdl_ttf', path + '\\include\\sdl', path + '\\include\\png', path + '\\include\\gettext', path + '\\include\\python'])
+	context.env.Append(LIBPATH = [path + '\\static_libs', path + '\\static_libs\\python'])
 	
 	context.env.Append(LIBS = ['mingw32', 'zlib', 'SDL_image', 'lua51', 'guichan', 'guichan_sdl', 'vorbis', 'ogg', 'vorbisfile', 'SDLmain', 'SDL', 'OpenAL32', 'SDL_ttf', 'boost_filesystem', 'boost_regex', 'png', 'ws2_32', 'intl', 'python25'])
 
