3df95a715ef03e615db1cd1713218e44444f6499
mvbarracuda <mvbarracuda@web.de>
2007-08-26 18:09:35 +0000

    * Updated batch files in build/win32 to work with the latest compile SDK

--- build/win32/update_project_files.bat
+++ build/win32/update_project_files.bat
@@ -1,9 +1,10 @@
-:: Users will need to change paths if they use custom MinGW or SCons version that didn't ship with the SDK
-set _=%CD%
+:: Users will need to change paths if they use custom MinGW, SCons, Python or SWIG versions that didn't ship with the SDK
+SET _=%CD%
+SET _SWIG="%_%\applications\swigwin-1.3.31"
 
 :: To avoid path collisions the following line was commented out and replaced with a slightly different version
 :: SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
-SET PATH=%_%\applications\scons;%_%\applications\mingw\bin
+SET PATH=%_%\applications\scons;%_%\applications\mingw\bin;\applications\python25
 
 :: Goto TRUNK and call SCons
 cd \
