2c8790bf038394d217becda8c312419bc90233c9
mvbarracuda <mvbarracuda@web.de>
2007-09-04 16:53:58 +0000

    * Minor batch file fix to bring back doxygen support on win32

--- build/win32/create_documentation.bat
+++ build/win32/create_documentation.bat
@@ -1,13 +1,9 @@
 :: Users will need to change paths if they use custom MinGW, SCons, Python or SWIG versions that didn't ship with the SDK
 SET _=%CD%
-SET _SWIG="%_%\applications\swigwin-1.3.31"
+SET PATH=%Path%;%_%\applications\scons;
 
-:: To avoid path collisions the following line was commented out and replaced with a slightly different version
-:: SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
-SET PATH=%_%\applications\scons;%_%\applications\mingw\bin;\applications\python25
-
-:: Delete engine_build_log.txt in case that it's still in place from a former compile sessions
-del %_%\engine_build_log.txt
+:: Delete doxygen_log.txt in case that it's still in place from a former compile sessions
+del %_%\doxygen_log.txt
 
 :: Goto TRUNK and call SCons
 cd \
