442634b9518ba78558ef5471a11b4d1fe60e915c
mvbarracuda <mvbarracuda@web.de>
2007-10-07 23:27:59 +0000

    * Adjusted linker order (the old order led to issues with minizip and zlib)  * Reintroducing code::blocks build support for win32; tested against one of the most recent C::B Win32 nightly builds  * Code::Blocks files kindly provided by ducbian :-)  * For some reason the code::blocks project builds fine but it brings up an runtime error if you start the techdemo python script; further investigation needed

--- build/win32-config-dist.py
+++ build/win32-config-dist.py
@@ -10,12 +10,13 @@ def addExtras(context):
 	context.env.Append(CPPPATH = [path, path + '\\include', path + '\\include\\openal', path + '\\include\\sdl_image', path + '\\include\\zlib', path + '\\include\\guichan', path + '\\include\\boost', path + '\\include\\libvorbis', path + '\\include\\sdl_ttf', path + '\\include\\sdl', path + '\\include\\png', path + '\\include\\python'])
 	context.env.Append(LIBPATH = [path + '\\static_libs', path + '\\static_libs\\python'])
 	
-	context.env.Append(LIBS = ['mingw32', 'zlib', 'SDL_image', 'guichan', 'guichan_sdl', 'vorbis', 'ogg', 'vorbisfile', 'SDLmain', 'SDL', 'OpenAL32', 'SDL_ttf', 'boost_filesystem', 'boost_regex', 'png', 'python25'])
+	context.env.Append(LIBS = ['guichan_sdl', 'guichan', 'mingw32', 'zlib', 'vorbis', 'ogg', 'vorbisfile', 'SDL_image', 'SDLmain', 'SDL', 'OpenAL32', 'SDL_ttf', 'boost_filesystem', 'boost_regex', 'png', 'python25'])
 
 	if context.env['opengl']:
 		context.env.Append(CPPPATH = [path + '\\include\\opengl'])
-		context.env.Append(LIBS = ['guichan_opengl', 'opengl32', 'glu32'])
+		context.env.Prepend(LIBS = ['guichan_opengl'])
+		context.env.Append(LIBS = ['opengl32', 'glu32'])
 
 	if context.env['zip']:
 		context.env.Append(CPPPATH = [path + '\\include\\minizip'])
-		context.env.Append(LIBS = ['minizip'])
+		context.env.Prepend(LIBS = ['minizip'])
