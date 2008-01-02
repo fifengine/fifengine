f5cd0eec84ec0f2468f168453899d7885e5ac530
mvbarracuda <mvbarracuda@web.de>
2008-01-02 17:05:45 +0000

    * Updated project files for win32 (MSVC 2005 & Code::Blocks)  * DLLs get copied to the new standalone editor client as well now (just run update_dlls_mingw.bat / update_dlls_msvc2005.bat again)

--- build/win32/update_dlls_mingw.bat
+++ build/win32/update_dlls_mingw.bat
@@ -1,7 +1,9 @@
+del .\..\..\clients\FIFEdit\*.dll
 del .\..\..\clients\fo_remake\*.dll
 del .\..\..\clients\island_demo\*.dll
 del .\..\..\clients\techdemo\*.dll
 
+copy .\binaries\mingw\*.dll .\..\..\clients\FIFEdit\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\fo_remake\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\island_demo\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\techdemo\*.*
--- build/win32/update_dlls_msvc2005.bat
+++ build/win32/update_dlls_msvc2005.bat
@@ -1,7 +1,9 @@
+del .\..\..\clients\FIFEdit\*.dll
 del .\..\..\clients\fo_remake\*.dll
 del .\..\..\clients\island_demo\*.dll
 del .\..\..\clients\techdemo\*.dll
 
+copy .\binaries\msvc2005\*.dll .\..\..\clients\FIFEdit\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\fo_remake\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\island_demo\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\techdemo\*.*
