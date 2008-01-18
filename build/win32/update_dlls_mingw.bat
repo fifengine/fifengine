69f432513d507808757d6e7c73613f3f8c8393bf
mvbarracuda <mvbarracuda@web.de>
2008-01-18 09:18:50 +0000

    * Win32 DLLs get copied to the pychan_demo client now as well  * Reverted island_demo setting to the former standard

--- build/win32/update_dlls_mingw.bat
+++ build/win32/update_dlls_mingw.bat
@@ -1,9 +1,11 @@
 del .\..\..\clients\FIFEdit\*.dll
 del .\..\..\clients\fo_remake\*.dll
 del .\..\..\clients\island_demo\*.dll
+del .\..\..\clients\pychan_demo\*.dll
 del .\..\..\clients\techdemo\*.dll
 
 copy .\binaries\mingw\*.dll .\..\..\clients\FIFEdit\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\fo_remake\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\island_demo\*.*
+copy .\binaries\mingw\*.dll .\..\..\clients\pychan_demo\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\techdemo\*.*
--- build/win32/update_dlls_msvc2005.bat
+++ build/win32/update_dlls_msvc2005.bat
@@ -1,9 +1,11 @@
 del .\..\..\clients\FIFEdit\*.dll
 del .\..\..\clients\fo_remake\*.dll
 del .\..\..\clients\island_demo\*.dll
+del .\..\..\clients\pychan_demo\*.dll
 del .\..\..\clients\techdemo\*.dll
 
 copy .\binaries\msvc2005\*.dll .\..\..\clients\FIFEdit\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\fo_remake\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\island_demo\*.*
+copy .\binaries\msvc2005\*.dll .\..\..\clients\pychan_demo\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\techdemo\*.*
