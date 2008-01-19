eb0b005e8b41fc2447b5f39d9d8fa7b55cdc7028
mvbarracuda <mvbarracuda@web.de>
2008-01-19 22:19:03 +0000

    * Batch files to build the boost unit tests on win32 with scons  * Caution: building these tests fails at the moment!

--- build/win32/build_environments/scons/logbuild_tests.bat
+++ build/win32/build_environments/scons/logbuild_tests.bat
@@ -6,10 +6,13 @@ SET _SWIG="%_%\..\..\applications\swigwin-1.3.31"
 :: SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
 SET PATH=%_%\..\..\applications\scons;%_%\..\..\applications\mingw\bin
 
+:: Delete engine_build_log.txt in case that it's still in place from a former compile sessions
+del "%_%\engine_build_log.txt"
+
 :: Goto TRUNK and call SCons
 cd \
 cd "%_%\..\..\..\.."
-scons debug=0 zip=1
+scons debug=0 zip=1 tests=1 >> "%_%\engine_build_log.txt" 2>&1
 
 :: Delete old versions of _fife.pyd and rename the compiled fife.dll to _fife.pyd
 cd "%_%\..\..\..\..\engine\swigwrappers\python"
