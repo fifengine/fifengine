c3340c51ced220036e0340050efe1673b3f2beda
mvbarracuda <mvbarracuda@web.de>
2008-01-26 06:41:09 +0000

    * Copy DLLs for win32 to the new editor application directory as well  * Run the update batch files for your compiler (mingw/msvc2005) at least once!

--- build/win32/update_dlls_mingw.bat
+++ build/win32/update_dlls_mingw.bat
@@ -1,12 +1,14 @@
 ren .\binaries\mingw\zlib.dll zlib1.dll
 
 del .\..\..\*.dll
+del .\..\..\clients\editor\*.dll
 del .\..\..\clients\FIFEdit\*.dll
 del .\..\..\clients\fo_remake\*.dll
 del .\..\..\clients\island_demo\*.dll
 del .\..\..\clients\pychan_demo\*.dll
 
 copy .\binaries\mingw\*.dll .\..\..\*.*
+copy .\binaries\mingw\*.dll .\..\..\clients\editor\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\FIFEdit\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\fo_remake\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\island_demo\*.*
