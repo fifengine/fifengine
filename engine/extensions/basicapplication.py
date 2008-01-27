bf422f861bb77e15cde7b8fe9aa5deac7e773939
phoku <klaus.blindert@web.de>
2008-01-27 19:38:53 +0000

    Documentation for basicapplication.

--- engine/extensions/basicapplication.py
+++ engine/extensions/basicapplication.py
@@ -1,20 +1,20 @@
 # coding: utf-8
+
 """
-ApplicationBase is an extendable class that provides a basic environment for a FIFE-based client.
+The basic application and main loop.
 
- The unextended application reads in and initializes engine settings, sets up a simple event
-listener, and pumps the engine while listening for a quit message. Specialized applications can
-modify settings.py to change initial engine settings. They can provide their own event listener
-by assigning to self.listenertype in their constructor. And they can override the _pump method
-to define runtime behavior of the application.
-
+See the L{ApplicationBase} documentation.
 """
 
 import fife
 import fifelog
 
-# Default, rudimentary event listener.
 class ExitEventListener(fife.IKeyListener):
+	"""
+	Default, rudimentary event listener.
+	
+	Will cause the application to quit on pressing ESC.
+	"""
 	def __init__(self, app):
 		self.app = app
 		self.engine = app.engine
@@ -33,6 +33,16 @@ class ExitEventListener(fife.IKeyListener):
 		pass
 
 class ApplicationBase(object):
+	"""
+	ApplicationBase is an extendable class that provides a basic environment for a FIFE-based client.
+	
+	The unextended application reads in and initializes engine settings, sets up a simple event
+	listener, and pumps the engine while listening for a quit message. Specialized applications can
+	modify settings.py to change initial engine settings. They can provide their own event listener
+	by overriding L{createListener}. And they can override the L{_pump} method
+	to define runtime behavior of the application.
+	
+	"""
 	def __init__(self):
 		self.engine = fife.Engine()
 
@@ -46,6 +56,10 @@ class ApplicationBase(object):
 		self.returnValues = []
 
 	def loadSettings(self):
+		"""
+		Load the settings from a python file and load them into the engine.
+		Called in the ApplicationBase constructor.
+		"""
 		import settings
 		self.settings = settings
 		
@@ -65,19 +79,40 @@ class ApplicationBase(object):
 		engineSetting.setScreenHeight(settings.ScreenHeight)
 	
 	def initLogging(self):
+		"""
+		Initialize the LogManager.
+		"""
 		self.log = fifelog.LogManager(self.engine, self.settings.LogToPrompt, self.settings.LogToFile)
 		if self.settings.LogModules:
 			self.log.setVisibleModules(*self.settings.LogModules)
 
 	def createListener(self):
+		"""
+		This creates a default event listener, which will just close the program
+		after pressing ESC.
+		
+		You should override this method to provide your own event handling.
+		"""
 		return ExitEventListener(self)
 
 	def run(self):
+		"""
+		Initialize the event listener and event loop - and start it.
+		"""
 		eventlistener = self.createListener()
 		self.engine.initializePumping()
 		return self.mainLoop()
 	
 	def mainLoop(self):
+		"""
+		The programs main loop.
+		
+		Do not override this, instead provide your own L{_pump} method.
+		You can call this recursively, e.g. to provide synchronous
+		Dialogs :-) and break out of the current mainLoop by calling
+		L{breakFromMainLoop}. It will return the argument passed
+		to L{breakFromMainLoop}.
+		"""
 		self.returnValues.append(None)
 		while not self.quitRequested:
 			try:
@@ -101,12 +136,25 @@ class ApplicationBase(object):
 		return self.returnValues.pop()
 
 	def breakFromMainLoop(self,returnValue):
+		"""
+		Break from the currently running L{mainLoop}.
+		
+		The passed argument will be returned by the mainLoop.
+		"""
 		self.returnValues[-1] = returnValue
 		self.breakRequested = True
 
-	# Application pump. Derived classes can specialize this for unique behavior.
+	
 	def _pump(self):
-		pass
+		"""
+		Application pump.
+		
+		Derived classes can specialize this for unique behavior.
+		This is called every frame.
+		"""
 
 	def quit(self):
+		"""
+		Quit the application. Really!
+		"""
 		self.quitRequested = True
