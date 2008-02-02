3958bf843c0596da684a44d50eb7d64a4d424551
mvbarracuda <mvbarracuda@web.de>
2008-02-02 11:14:29 +0000

    * Good looking bitmap font with black borders around the font glyphs for island_demo

--- build/win32/update_dlls_mingw.bat
+++ build/win32/update_dlls_mingw.bat
@@ -2,14 +2,12 @@ ren .\binaries\mingw\zlib.dll zlib1.dll
 
 del .\..\..\*.dll
 del .\..\..\clients\editor\*.dll
-del .\..\..\clients\FIFEdit\*.dll
 del .\..\..\clients\fo_remake\*.dll
 del .\..\..\clients\island_demo\*.dll
 del .\..\..\clients\pychan_demo\*.dll
 
 copy .\binaries\mingw\*.dll .\..\..\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\editor\*.*
-copy .\binaries\mingw\*.dll .\..\..\clients\FIFEdit\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\fo_remake\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\island_demo\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\pychan_demo\*.*
--- build/win32/update_dlls_msvc2005.bat
+++ build/win32/update_dlls_msvc2005.bat
@@ -1,13 +1,11 @@
 del .\..\..\*.dll
 del .\..\..\clients\editor\*.dll
-del .\..\..\clients\FIFEdit\*.dll
 del .\..\..\clients\fo_remake\*.dll
 del .\..\..\clients\island_demo\*.dll
 del .\..\..\clients\pychan_demo\*.dll
 
 copy .\binaries\msvc2005\*.dll .\..\..\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\editor\*.*
-copy .\binaries\msvc2005\*.dll .\..\..\clients\FIFEdit\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\fo_remake\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\island_demo\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\pychan_demo\*.*
