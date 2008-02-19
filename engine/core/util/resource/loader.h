dddf08b9dea9590059299d23617c9f30278dff68
jwt <jthickstun@gmail.com>
2008-02-19 20:12:44 +0000

    Interface definitions for new resource system.

---
+++ engine/core/util/resource/loader.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2007 by the FIFE Team                              *
+ *   fife-public@lists.sourceforge.net                                     *
+ *   This file is part of FIFE.                                            *
+ *                                                                         *
+ *   FIFE is free software; you can redistribute it and/or modify          *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA              *
+ ***************************************************************************/
+
+#ifndef FIFE_LOADER_H
+#define FIFE_LOADER_H
+
+// Standard C++ library includes
+
+// 3rd party library includes
+
+// FIFE includes
+// These includes are split up in two parts, separated by one empty line
+// First block: files included from the FIFE root src directory
+// Second block: files included from the same folder
+
+namespace FIFE {
+
+	/** ILoader defines an interface for loading resources. The resource returned is owned by
+	 * the caller, and must be deleted when finished.
+	 */
+	class ILoader {
+	public:
+		virtual ~ILoader() { };
+
+		virtual IResource* load(const ResourceLocation& location) = 0;
+	};
+}
+
+#endif
---
+++ engine/core/util/resource/resource.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2007 by the FIFE Team                              *
+ *   fife-public@lists.sourceforge.net                                     *
+ *   This file is part of FIFE.                                            *
+ *                                                                         *
+ *   FIFE is free software; you can redistribute it and/or modify          *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA              *
+ ***************************************************************************/
+
+#ifndef FIFE_RESOURE_H
+#define FIFE_RESOURE_H
+
+// Standard C++ library includes
+
+// 3rd party library includes
+
+// FIFE includes
+// These includes are split up in two parts, separated by one empty line
+// First block: files included from the FIFE root src directory
+// Second block: files included from the same folder
+#include "resource_location.h"
+
+namespace FIFE {
+
+	/** IResource is the internal representation of a loaded file.
+	 * One resource is always associated with one file (resource location).
+	 */
+	class IResource {
+	public:
+		virtual ~IResource() { };
+
+		/** Get the location of this resource.
+		 */
+		virtual const ResourceLocation& getLocation() = 0;
+
+		/** Change the location of this resource.
+		 */
+		virtual void setLocation(const ResourceLocation& location) = 0;
+	};
+}
+
+#endif
