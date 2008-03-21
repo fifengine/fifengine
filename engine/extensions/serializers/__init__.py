27497acdfa9072c94096ffe0dd518f70f31460c1
jasoka <j.jasoka@gmail.com>
2008-03-21 14:27:08 +0000

    Still improved loader error reporting. Adds 'guarded'-decorator to serializers/__init__.py -> prints exception type, filename and xmldump of the file. Also converts from fife exceptions to serializer exceptions. Functionality added to object & animation loaders.

---
+++ engine/extensions/serializers/__init__.py
@@ -0,0 +1,56 @@
+import fife, sys
+from traceback import print_exc
+
+__all__ = ('ET', 'SerializerError', 'InvalidFormat', 'NameClash', 'NotFound', 'guarded', 'warn')
+
+try:
+	import xml.etree.cElementTree as ET
+except:
+	import xml.etree.ElementTree as ET
+
+class SerializerError(Exception):
+	pass
+
+class InvalidFormat(SerializerError):
+	pass
+
+class NameClash(SerializerError):
+	pass
+
+class NotFound(SerializerError):
+	pass
+
+def guarded(fn):
+	'''
+	Use "guadred" decorator to catch serializer exceptions, and print more nice output before exception is re-raised
+	Usage of decorator benefits if instance of class where decorated method resides, contains attributes
+	self.filename (currently processed filename) and self.node (ET node currently under work).
+	In case these are in place, filename gets printed and node dumped along with the exception info
+	'''
+	def wrapped(self, *args, **kwargs):
+		try:
+			result = fn(self, *args, **kwargs)
+			return result
+		except:
+			print "\n\n=================== SERIALIZER EXCEPTION INFO BEGIN ======================"
+			print "Exception : ", sys.exc_info()[0]
+			if hasattr(self, 'filename'):
+				print "Error while processing file " + self.filename
+				print "----------------------------------------------------------------"
+			print 'Traceback:'
+			print_exc()
+			if hasattr(self, 'node') and self.node:
+				print "----------------------------------------------------------------"
+				print "XML dump:"
+				ET.dump(self.node)
+			print "==================== SERIALIZER EXCEPTION INFO END =======================\n\n"
+			# re-raise fife exceptions as native python exceptions, otherwise python seems to barf
+			# about "cannot raise type..."
+			if type(sys.exc_info()[0]) == type:
+				raise SerializerError(sys.exc_info()[1])
+			else:
+				raise
+	return wrapped
+
+def warn(self, msg):
+	print 'Warning (%s): %s' % (self.filename, msg)
--- engine/extensions/serializers/xmlobject.py
+++ engine/extensions/serializers/xmlobject.py
@@ -1,10 +1,5 @@
-import fife, sys
-from traceback import print_exc
-
-try:
-	import xml.etree.cElementTree as ET
-except:
-	import xml.etree.ElementTree as ET
+import fife
+from serializers import *
 
 class ObjectLocation(fife.ResourceLocation):
 	def __init__(self, file, node=None):
@@ -20,76 +15,50 @@ class XMLObjectLoader(fife.ObjectLoader):
 		self.dataset=dataset
 		self.vfs = vfs
 		self.source = None
+		self.filename = ''
 
 	def loadResource(self, location):
+		# store information about stuff to be loaded, after that do actual loading under @guarded
 		self.source = location
-		node = None
-		try:
-			node = location.node
-		except AttributeError:
-			pass
-
-		if node == None:
-			try:
-				f = self.vfs.open(self.source)
-			except AttributeError:
-				print 'XMLObjectLoader was asked to open a file %s, but no vfs was given.' % self.source.getFilename()
-				raise
+		self.filename = self.source.getFilename()
+		self.node = None
+		if hasattr(location, 'node'):
+			self.node = location.node
+		self.do_load_resource()
+	
+	@guarded
+	def do_load_resource(self):
+		if self.node == None:
+			f = self.vfs.open(self.source)
 			tree = ET.parse(f)
-			node = tree.getroot()
-			if node.tag != 'object':
-				self._err(''.join(['Expected <object> tag, but found <', node.tag, '>.']))
-
-		try:
-			self.parse_object(self.dataset, node)
-		except:
-			print "\n\n=================== LOADER EXCEPTION INFO BEGIN ======================"
-			print "Exception : ", sys.exc_info()[0]
-			print "Error while processing file " + self.source.getFilename()
-			print "----------------------------------------------------------------"
-			print_exc()
-			print "----------------------------------------------------------------"
-			print "XML dump:"
-			ET.dump(node)
-			print "==================== LOADER EXCEPTION INFO END =======================\n\n"
-			raise
+			self.node = tree.getroot()
+		self.parse_object(self.dataset, self.node)
 
 	def parse_object(self, dataset, object):
+		if self.node.tag != 'object':
+			raise InvalidFormat('Expected <object> tag, but found <%s>.' % node.tag)
+		
 		id = object.get('id')
-		parent = object.get('parent')
-		blocking = object.get('blocking')
-		static = object.get('static')
-		pather = object.get('pather')
-
-		if not pather: pather = self.model.getPather('RoutePather')
-
-		if not id: self._err('<object> declared without an id attribute.')
-
+		if not id:
+			raise InvalidFormat('<object> declared without an id attribute.')
+		
 		obj = None
+		parent = object.get('parent', None)
 		if parent:
-			try:
-				query = self.metamodel.getObjects('id', str(parent))
-				if len(query) == 0: self._err(''.join(['No objects found with identifier', str(parent), '.']))
-				if len(query) > 1: 
-					self._print_err('Warning: %d objects found with identifier %s.' % (str(len(query)), str(parent)))
-				parent = query[0]
+			query = self.metamodel.getObjects('id', str(parent))
+			if len(query) == 0:
+				raise NotFound('No objects found with identifier %s.' % str(parent))
+			elif len(query) > 1:
+				raise NameClash('%d objects found with identifier %s.' % (len(query), str(parent)))
+			parent = query[0]
+		obj = dataset.createObject(str(id), parent)
+		fife.ObjectVisual.create(obj)
 
-				obj = dataset.createObject(str(id), parent)
-				fife.ObjectVisual.create(obj)
-			except fife.Exception, e:
-				self._print_err(e.getMessage() + '\n\tThe object ' + str(id) + ' already exists! Ignoring object definition.')
-				return
-		else:
-			try:
-				obj = dataset.createObject(str(id))
-				fife.ObjectVisual.create(obj)
-			except fife.Exception, e:
-				self._print_err(e.getMessage() + '\n\tThe object ' + str(id) + ' already exists! Ignoring object definition.')
-				return
-
-		obj.setBlocking(bool(blocking))
-		obj.setStatic(bool(static))
-		obj.setPather(pather)
+		obj.setBlocking(bool( object.get('blocking', False) ))
+		obj.setStatic(bool( object.get('static', False) ))
+		
+		pather = object.get('pather', 'RoutePather')
+		obj.setPather( self.model.getPather(pather) )
 
 		self.parse_images(object, obj)
 		self.parse_actions(object, obj)
@@ -98,24 +67,20 @@ class XMLObjectLoader(fife.ObjectLoader):
 		for image in objelt.findall('image'):
 			source = image.get('source')
 			if not source:
-				self._err('<image> declared without a source attribute.')
-			
-			direction = image.get('direction', 0)
-			x_offset = image.get('x_offset', 0)
-			y_offset = image.get('y_offset', 0)
+				raise InvalidFormat('<image> declared without a source attribute.')
 
-			id = self.image_pool.addResourceFromFile(str(source))	
-			object.get2dGfxVisual().addStaticImage(int(direction), id)
+			id = self.image_pool.addResourceFromFile( str(source) )
+			object.get2dGfxVisual().addStaticImage(int( image.get('direction', 0) ), id)
 			img = self.image_pool.getImage(id)
-			img.setXShift(int(x_offset))
-			img.setYShift(int(y_offset))
+			img.setXShift(int( image.get('x_offset', 0) ))
+			img.setYShift(int( image.get('y_offset', 0) ))
 
 	def parse_actions(self, objelt, object):
 		for action in objelt.findall('action'):
 			id = action.get('id')
-			if not id: self._err('<action> declared without an id attribute.')
+			if not id:
+				raise InvalidFormat('<action> declared without an id attribute.')
 	
-			act_obj = None
 			act_obj = object.createAction(str(id))
 			fife.ActionVisual.create(act_obj)
 			self.parse_animations(action, act_obj)
@@ -123,12 +88,11 @@ class XMLObjectLoader(fife.ObjectLoader):
 	def parse_animations(self, actelt, action):
 		for anim in actelt.findall('animation'):
 			source = anim.get('source')
-			if not source: 
-				self._err('Animation declared with no source location.')
+			if not source:
+				raise InvalidFormat('Animation declared with no source location.')
 			
-			direction = anim.get('direction', 0)
 			anim_id = self.anim_pool.addResourceFromFile(str(source))
 			animation = self.anim_pool.getAnimation(anim_id)
-			action.get2dGfxVisual().addAnimation(int(direction), anim_id)
+			action.get2dGfxVisual().addAnimation(int( anim.get('direction', 0) ), anim_id)
 			action.setDuration(animation.getDuration())
 
