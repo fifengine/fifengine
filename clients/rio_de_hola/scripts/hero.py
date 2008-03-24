43c78439347d580c981350859443bd6910116dbe
mvbarracuda <mvbarracuda@web.de>
2008-03-24 02:43:41 +0000

    * Unpacked rio de hola package  * Removed original zipped version  * LICENSE file for rio de hola client

---
+++ clients/rio_de_hola/scripts/hero.py
@@ -0,0 +1,40 @@
+import random
+from agent import Agent
+import settings as TDS
+
+_STATE_NONE, _STATE_IDLE, _STATE_RUN, _STATE_KICK, _STATE_TALK = xrange(5)
+
+class Hero(Agent):
+	def __init__(self, model, agentName, layer, uniqInMap=True):
+		super(Hero, self).__init__(model, agentName, layer, uniqInMap)
+		self.state = _STATE_NONE
+		self.idlecounter = 1
+
+	def onInstanceActionFinished(self, instance, action):
+		self.idle()
+		if action.Id() != 'idle':
+			self.idlecounter = 1
+		else:
+			self.idlecounter += 1
+		if self.idlecounter % 7 == 0:
+			txtindex = random.randint(0, len(TDS.heroTexts) - 1)
+			instance.say(TDS.heroTexts[txtindex], 2500)
+	
+	def start(self):
+		self.idle()
+	
+	def idle(self):
+		self.state = _STATE_IDLE
+		self.agent.act('idle', self.agent.getFacingLocation())
+		
+	def run(self, location):
+		self.state = _STATE_RUN
+		self.agent.move('run', location, 4 * TDS.TestAgentSpeed)
+	
+	def kick(self, target):
+		self.state = _STATE_KICK
+		self.agent.act('kick', target)
+		
+	def talk(self, target):
+		self.state = _STATE_TALK
+		self.agent.act('talk', target)
---
+++ clients/rio_de_hola/settings.py
@@ -0,0 +1,45 @@
+BitsPerPixel        = 0
+FullScreen          = 0
+InitialVolume       = 5.0
+PlaySounds          = 1
+RenderBackend       = "OpenGL"
+SDLRemoveFakeAlpha  = 1
+ScreenWidth         = 1024
+ScreenHeight        = 768
+MapFile             = 'content/maps/island_demo_map.xml'
+Font                = 'content/fonts/samanata.ttf'
+FontGlyphs          = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/():;%&`'*#=[]\""
+
+LogModules          = ['controller']
+PychanDebug         = False
+LogToPrompt         = 1
+LogToFile           = 0
+UsePsyco            = False
+ImageChunkSize      = 256
+
+CoordinateLayerName = "TechdemoMapObjectLayer"
+QuadTreeLayerName = "" #"TechdemoMapObjectLayer"
+
+heroTexts = (
+	'I wonder when the guys will\n' + 'get this world ready for me',
+	"Bring it on baby,\nI'm ready for action!",
+	'humm di dum....',
+)
+
+girlTexts = (
+	"Why I am running? Haven't you heard\nthat the bees are coming?!",
+	'Heeelp!!! The bees are going to get us!',
+)
+
+beekeeperTexts = (
+	"Some idiot has grown mutated bees around here",
+	'Want to take a look at my farm?',
+	'Did you know that I actually cannot do anything\nelse than stand still and greet everybody?',
+)
+
+
+# test settings
+TestCameraPlacement = 0
+TestRotationLayerName = "TechdemoMapObjectLayer" # or "TechdemoMapTileLayer"
+TestAgentSpeed      = 0.5
+
