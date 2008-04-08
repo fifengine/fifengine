7067a22b6d967e1899740982fecf56259dc70e61
mvbarracuda <mvbarracuda@web.de>
2008-04-08 13:22:27 +0000

    * Quick and dirty fix for the zlib.dll vs zlib1.dll issue  * Further investigation is needed why FIFE tries to call zlib1.dll; first tests with ollydbg show that FIFE tries to access zlib.dll & zlib1.dll though _fife.pyd just contains calls to zlib.dll; this might be caused by a 3rd party DLL e.g. physfs.dll

--- build/win32/update_dlls_mingw.bat
+++ build/win32/update_dlls_mingw.bat
@@ -3,6 +3,7 @@ del .\..\..\clients\editor\*.dll
 del .\..\..\clients\island_demo\*.dll
 del .\..\..\clients\pychan_demo\*.dll
 
+copy .\binaries\mingw\zlib.dll .\binaries\mingw\zlib1.dll
 copy .\binaries\mingw\*.dll .\..\..\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\editor\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\island_demo\*.*
