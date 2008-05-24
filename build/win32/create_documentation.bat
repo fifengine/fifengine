19412457634e27793955229322d90d9aa6a2fe90
mvbarracuda <mvbarracuda@web.de>
2008-05-24 12:03:56 +0000

    * Updated build-related scripts / files to work with the latest compile SDK release 2008.0-r3  * Grab the new compile SDK here: http://downloads.sourceforge.net/fife/FIFE_2008.0-r3_win32_SDK.7z  * List of compile SDK changes:   * Updated SWIG from 1.3.34 to 1.3.35.   * Rebuilt libtiff & libfreetype2 to remove the zlib1.dll dependency.   * Removed libpng.dll from MSVC2008 binaries folder.   * Replaced openal_soft with original openal32 implementation from Creative Labs.

--- build/win32/create_documentation.bat
+++ build/win32/create_documentation.bat
@@ -1,6 +1,6 @@
 :: Users will need to change paths if they use custom MinGW, SCons, Python or SWIG versions that didn't ship with the SDK
 SET _=%CD%
-SET _SWIG="%_%\applications\swigwin-1.3.34"
+SET _SWIG="%_%\applications\swigwin-1.3.35"
 
 :: To avoid path collisions the following line was commented out and replaced with a slightly different version
 :: SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
--- build/win32/update_project_files.bat
+++ build/win32/update_project_files.bat
@@ -1,10 +1,16 @@
 :: Users will need to change paths if they use custom MinGW, SCons, Python or SWIG versions that didn't ship with the SDK
 SET _=%CD%
-SET _SWIG="%_%\applications\swigwin-1.3.34"
+SET _SWIG="%_%\applications\swigwin-1.3.35"
 
 :: To avoid path collisions the following line was commented out and replaced with a slightly different version
 :: SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
 SET PATH=%_%\applications\scons;%_%\applications\mingw\bin
+
+:: Delete old generated project files to ensure that they get generated from scratch again
+cd %_%\build_environments
+del code_blocks\fife_engine.cbp
+del visual_studio_8\fife.vcproj
+del visual_studio_9\fife.vcproj
 
 :: Goto TRUNK and call SCons
 cd \
