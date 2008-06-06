5a05e33714eb03d265dcfdfcd723c0f85822492c
mvbarracuda <mvbarracuda@web.de>
2008-06-06 17:19:31 +0000

    * Removed zlib workaround (not needed anymore with the 2008.0-r3 SDK)

--- build/win32/update_dlls_mingw.bat
+++ build/win32/update_dlls_mingw.bat
@@ -3,7 +3,6 @@ del .\..\..\clients\editor\*.dll
 del .\..\..\clients\pychan_demo\*.dll
 del .\..\..\clients\rio_de_hola\*.dll
 
-copy .\binaries\mingw\zlib.dll .\binaries\mingw\zlib1.dll
 copy .\binaries\mingw\*.dll .\..\..\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\editor\*.*
 copy .\binaries\mingw\*.dll .\..\..\clients\pychan_demo\*.*
