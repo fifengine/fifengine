6e8fe3ee6970b84ab31e967349cb3831e24dce08
mvbarracuda <mvbarracuda@web.de>
2008-06-29 18:29:36 +0000

    * Getting rid of trunk to make the datasets branch the new trunk as next step

--- build/win32/update_dlls_mingw.bat
+++
@@ -1,9 +0,0 @@
-del .\..\..\*.dll
-del .\..\..\clients\editor\*.dll
-del .\..\..\clients\island_demo\*.dll
-del .\..\..\clients\pychan_demo\*.dll
-
-copy .\binaries\mingw\*.dll .\..\..\*.*
-copy .\binaries\mingw\*.dll .\..\..\clients\editor\*.*
-copy .\binaries\mingw\*.dll .\..\..\clients\island_demo\*.*
-copy .\binaries\mingw\*.dll .\..\..\clients\pychan_demo\*.*
--- engine/extensions/editor/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-
-from fifedit import Fifedit
-from selection import Selection
-
-__all__ = [ 'fifedit', 'selection', 'plugins' ]
