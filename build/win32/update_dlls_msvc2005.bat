643bd18f9d0f471026eb8682f7b692d8c28970db
mvbarracuda <mvbarracuda@web.de>
2008-07-06 21:38:38 +0000

    * Copying DLLs to the right folders again  * Thanks for the hint Humpie

--- build/win32/update_dlls_msvc2005.bat
+++ build/win32/update_dlls_msvc2005.bat
@@ -5,5 +5,5 @@ del .\..\..\clients\pychan_demo\*.dll
 
 copy .\binaries\msvc2005\*.dll .\..\..\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\editor\*.*
-copy .\binaries\msvc2005\*.dll .\..\..\clients\island_demo\*.*
 copy .\binaries\msvc2005\*.dll .\..\..\clients\pychan_demo\*.*
+copy .\binaries\msvc2005\*.dll .\..\..\clients\rio_de_hola\*.*
--- build/win32/update_dlls_msvc2008.bat
+++ build/win32/update_dlls_msvc2008.bat
@@ -5,5 +5,5 @@ del .\..\..\clients\pychan_demo\*.dll
 
 copy .\binaries\msvc2008\*.dll .\..\..\*.*
 copy .\binaries\msvc2008\*.dll .\..\..\clients\editor\*.*
-copy .\binaries\msvc2008\*.dll .\..\..\clients\island_demo\*.*
 copy .\binaries\msvc2008\*.dll .\..\..\clients\pychan_demo\*.*
+copy .\binaries\msvc2008\*.dll .\..\..\clients\rio_de_hola\*.*
