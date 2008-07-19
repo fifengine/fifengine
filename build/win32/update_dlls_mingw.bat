d81edb2c92c18569822562d166f32450dc148a93
sleek <dean98@gmail.com>
2008-07-19 10:43:42 +0000

    Layer::addInstance for moving instances around layers. Useful when we have multi-level buildings

--- build/win32/update_dlls_mingw.bat
+++
@@ -1,9 +0,0 @@
-del .\..\..\*.dll
-del .\..\..\clients\editor\*.dll
-del .\..\..\clients\pychan_demo\*.dll
-del .\..\..\clients\rio_de_hola\*.dll
-
-copy .\binaries\mingw\*.dll .\..\..\*.*
-copy .\binaries\mingw\*.dll .\..\..\clients\editor\*.*
-copy .\binaries\mingw\*.dll .\..\..\clients\pychan_demo\*.*
-copy .\binaries\mingw\*.dll .\..\..\clients\rio_de_hola\*.*
--- build/win32/update_dlls_msvc2005.bat
+++
@@ -1,9 +0,0 @@
-del .\..\..\*.dll
-del .\..\..\clients\editor\*.dll
-del .\..\..\clients\island_demo\*.dll
-del .\..\..\clients\pychan_demo\*.dll
-
-copy .\binaries\msvc2005\*.dll .\..\..\*.*
-copy .\binaries\msvc2005\*.dll .\..\..\clients\editor\*.*
-copy .\binaries\msvc2005\*.dll .\..\..\clients\pychan_demo\*.*
-copy .\binaries\msvc2005\*.dll .\..\..\clients\rio_de_hola\*.*
