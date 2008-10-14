3a24bdeda757af5b549817b386782574fef1f9bf
phoku <klaus.blindert@web.de>
2008-10-14 20:20:29 +0000

    Fixed a tiny off-by-one in the getBixel function.

--- engine/core/video/image.cpp
+++ engine/core/video/image.cpp
@@ -108,7 +108,7 @@ namespace FIFE {
 	}
 
 	void Image::getPixelRGBA(int x, int y, uint8_t* r, uint8_t* g, uint8_t* b, uint8_t* a) {
-		if ((x < 0) || (x >= m_surface->w) || (y < 0) || (y >= m_surface->h)) {
+		if ((x < 0) || (x > m_surface->w) || (y < 0) || (y > m_surface->h)) {
 			r = 0;
 			g = 0;
 			b = 0;
