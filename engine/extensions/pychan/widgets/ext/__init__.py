174fa4cfc6eea822a4f6f5287e883d4c387eee8f
phoku <klaus.blindert@web.de>
2009-03-26 17:13:03 +0000

    Extension widgets 'namespace' created.

---
+++ engine/extensions/pychan/widgets/ext/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Pychan extension widgets.
+
+Extension widgets are partly experimental, partly rarely used widgets
+which are added here. They are by default not included in the widgets
+registry and thus cannot be loaded from XML files. Use L{pychan.widgets.registerWidget}
+to enable that.
+
+Not the same care to keep the API stable will be taken for them and
+before and if they are added (or replace) the standard widgets they
+will have to be reviewed in detail.
+"""
