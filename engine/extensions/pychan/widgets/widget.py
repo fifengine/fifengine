16fb410eb52cceb1d2fa679efe3d34209876e14d
nihathrael <thomas.kinnen@gmail.com>
2009-05-08 08:45:27 +0000

    * Added patch by MadMonk that exposes the guichan focus functions to pychan * Thanks for contributing

--- engine/extensions/pychan/widgets/widget.py
+++ engine/extensions/pychan/widgets/widget.py
@@ -59,7 +59,7 @@ class Widget(object):
 		PointAttr('min_size'), PointAttr('size'), PointAttr('max_size'),
 		ColorAttr('base_color'),ColorAttr('background_color'),ColorAttr('foreground_color'),ColorAttr('selection_color'),
 		Attr('style'), Attr('font'),IntAttr('border_size'),Attr('position_technique'),
-		UnicodeAttr('helptext')
+		UnicodeAttr('helptext'), BoolAttr('is_focusable') 
 		]
 
 	DEFAULT_NAME = '__unnamed__'
@@ -641,6 +641,10 @@ class Widget(object):
 	def _getName(self): return self._name
 	name = property(_getName,_setName)
 
+	def _setFocusable(self, b): self.real_widget.setFocusable(b)
+	def _isFocusable(self):
+		return self.real_widget.isFocusable()
+
 	x = property(_getX,_setX)
 	y = property(_getY,_setY)
 	width = property(_getWidth,_setWidth)
@@ -649,6 +653,7 @@ class Widget(object):
 	position = property(_getPosition,_setPosition)
 	font = property(_getFont,_setFont)
 	border_size = property(_getBorderSize,_setBorderSize)
+	is_focusable = property(_isFocusable,_setFocusable) 
 
 	def setEnterCallback(self, cb):
 		"""
