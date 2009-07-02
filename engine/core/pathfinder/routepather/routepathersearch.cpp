479198d7d71a3f25828c5e0eb379858394a5adaa
vtchill <manning.jesse@gmail.com>
2009-07-02 02:14:12 +0000

    removed the deleted of the heuristic in the RoutePatherSearch class. Don't want to delete the heuristic since it is holding a pointer to a singleton object. If this is ever not holding a pointer returned by a singleton then it will leak since it is not being deleted.

--- engine/core/pathfinder/routepather/routepathersearch.cpp
+++ engine/core/pathfinder/routepather/routepathersearch.cpp
@@ -57,7 +57,6 @@ namespace FIFE {
 
         RoutePatherSearch::~RoutePatherSearch(){
                  delete m_searchspace;
-                 delete m_heuristic;
         }
 
 	void RoutePatherSearch::updateSearch() {
