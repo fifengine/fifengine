5a084b61aef9469085bfe06c4112e45a6f80e464
fifengine <fife@fifengine.net>
2009-08-14 00:00:58 +0000

    merged the pathfinding branch to trunk

--- engine/core/pathfinder/routepather/routepathersearch.h
+++ engine/core/pathfinder/routepather/routepathersearch.h
@@ -30,7 +30,6 @@
 // These includes are split up in two parts, separated by one empty line
 // First block: files included from the FIFE root src directory
 // Second block: files included from the same folder
-#include "pathfinder/search.h"
 #include "util/structures/priorityqueue.h"
 
 namespace FIFE {
@@ -43,32 +42,96 @@ namespace FIFE {
 	 *
 	 * For now this class uses offline A*, however eventually this will be switched over to RTA*.
 	 */
-	class RoutePatherSearch : public Search {
+	class RoutePatherSearch {
 	public:
 		RoutePatherSearch(const int session_id, const Location& from, const Location& to, SearchSpace* searchSpace);
+
+                typedef std::list<Location> Path;
+                /** An enumeration of the different status the search can be in.
+                 *
+                 */
+                enum SearchStatus {
+                        search_status_failed,
+                        search_status_complete,
+                        search_status_incomplete
+                };
 
 		virtual void updateSearch();
 
 		virtual Path calcPath();
-	private:
-		//The class to use to calculate the heuristic value.
-		Heuristic*                m_heuristic;
+
+                /** Retrieves the session id.
+                 *
+                 * @return The searches session id in the pather.
+                 */
+                int getSessionId() const {
+                        return m_sessionId;
+                }
+
+                /** Retrieves the pather.
+                 *
+                 * @return A pointer to the abstract pather which
+                 */
+                SearchSpace* getSearchSpace() const {
+                        return m_searchspace;
+                }
+
+                /** A small function which returns the current status of the search.
+                 *
+                 * @return An integer value representing the status, which is enumerated by this class.
+                 */
+                int getSearchStatus() const {
+                        return m_status;
+                }
+
+         protected:
+                /** Sets the current status of the search.
+                 *
+                 * @param status The status to set.
+                 */
+                void setSearchStatus(const SearchStatus status) {
+                        m_status = status;
+                }
+
+         private:
+                //A location object representing where the search started.
+                Location                m_to;
+
+                //A location object representing where the search ended.
+                Location                m_from;
+
+                //An integer containing the session id for this search.
+                int                             m_sessionId;
+
+                //A pointer to the pather that owns this search.
+                SearchSpace*    m_searchspace;
+
+                //An enumeration of the searches current status.
+                SearchStatus    m_status;
+
+                //The class to use to calculate the heuristic value.
+                Heuristic*                m_heuristic;
+
 		//The destination coordinate as an int.
 		int                       m_destCoordInt;
+
 		//The start coordinate as an int.
 		int                       m_startCoordInt;
+
 		//The next coordinate int to check out.
 		int                       m_next;
+
 		//The shortest path tree.
 		std::vector<int>          m_spt;
+
 		//The search frontier.
 		std::vector<int>	      m_sf;
+
 		//A table to hold the costs.
 		std::vector<float>		  m_gCosts;
+
 		//priority queue to hold nodes on the sf in order. 
 		PriorityQueue<int, float> m_sortedfrontier;
 	};
-
 }
-
 #endif
