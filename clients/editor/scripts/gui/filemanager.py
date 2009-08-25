f1ee37d61994bd4537c8c8a7b9251484f0cca477
cheesesucker <cheesesucker@sinnsyk.com>
2009-08-25 01:21:00 +0000

    Several changes to layer, camera and map dialogs:  * Renamed CameraEditor and LayerEditor to CameraDialog and LayerDialog  * Moved CameraDialog and LayerDialog into their own modules in scripts.gui so other components can use them.  * Prettier camera dialog  * Added LayerDialog to map wizard

--- clients/editor/scripts/gui/filemanager.py
+++ clients/editor/scripts/gui/filemanager.py
@@ -9,6 +9,8 @@ from action import Action, ActionGroup
 from input import InputDialog
 from selection import SelectionDialog, ClickSelectionDialog
 from scripts.events import events
+from scripts.gui.cameradialog import CameraDialog
+from scripts.gui.layerdialog import LayerDialog
 
 class FileManager(object):
 	def __init__(self):
@@ -119,18 +121,20 @@ class FileManager(object):
 			self._newMap()
 
 	def _newMap(self):
-		self._mapdlg = InputDialog(u'Enter a map identifier:', self._newLayer, self._clean)
+		self._mapdlg = InputDialog(u'Enter a map name:', self._newLayer, self._clean)
 		
 	def _newLayer(self, mapId):
+		if mapId == '':
+			print "Please enter a map ID"
+			return self._newMap()
+			
 		self._map = self.engine.getModel().createMap(str(mapId))
-		self._layerdlg = InputDialog(u'Enter a layer identifier for a default layer:', self._newCamera, self._clean)
+		self._layerdlg = LayerDialog(self.engine, self._map, self._newCamera, self._clean)
 		
-	def _newCamera(self, layerId):
-		grid = fife.SquareGrid()
-		self._layer = self._map.createLayer(str(layerId), grid)
-		grid.thisown = 0
+	def _newCamera(self, layer):
+		self._layer = layer
 		
-		self._cameradlg = CameraEditor(self.engine, self._addMap, self._clean, self._map, self._layer)
+		self._cameradlg = CameraDialog(self.engine, self._addMap, self._clean, self._map, self._layer)
 		
 	def _addMap(self):
 		self.editor.newMapView(self._map)
@@ -205,88 +209,3 @@ class FileManager(object):
 			
 		events.onObjectsImported.send(sender=self)
 
-class CameraEditor(object):
-	"""
-	CameraEditor provides a gui dialog for camera creation. The callback is called when camera creation is complete. A
-	partial specification of the camera parameters may optionally be given.
-	"""
-	def __init__(self, engine, callback=None, onCancel=None, map=None, layer=None):
-		self.engine = engine
-		self.callback = callback
-		self.onCancel = onCancel
-		self._widget = pychan.loadXML('gui/cameraedit.xml')
-
-		if map:
-			self._widget.distributeData({
-				'mapBox'	: unicode(map.getId()),
-			})
-
-		if layer:
-			self._widget.distributeData({
-				'layerBox'	: unicode(layer.getId()),
-			})
-
-		self._widget.mapEvents({
-			'okButton'     : self._finished,
-			'cancelButton' : self._cancelled
-		})
-
-		self._widget.show()
-		
-	def _cancelled(self):
-		if self.onCancel:
-			self.onCancel()
-		self._widget.hide()
-		
-
-	def _finished(self):
-		id = self._widget.collectData('idBox')
-		if id == '':
-			print 'Please enter a camera id.'
-			return
-
-		try:
-			map = self.engine.getModel().getMap(str(self._widget.collectData('mapBox')))
-		except fife.Exception:
-			print 'Cannot find the specified map id.'
-			return
-
-		try:
-			layer = map.getLayer(str(self._widget.collectData('layerBox')))
-		except fife.Exception:
-			print 'Cannot find the specified layer id.'	
-			return
-
-		try:
-			vals = self._widget.collectData('viewBox').split(',')
-			if len(vals) != 4:
-				raise ValueError	
-
-			viewport = fife.Rect(*[int(c) for c in vals])
-		except ValueError:
-			print 'Please enter 4 comma (,) delimited values for viewport x,y,width,height.'
-			return
-
-		try:
-			refh = int(self._widget.collectData('refhBox'))
-			refw = int(self._widget.collectData('refwBox'))
-		except ValueError:
-			print 'Please enter positive integer values for reference width and height.'
-			return
-
-		try:
-			rot = int(self._widget.collectData('rotBox'))
-			tilt = int(self._widget.collectData('tiltBox'))
-		except ValueError:
-			print 'Please enter positive integer values for rotation and tilt.'
-			return
-
-		cam = self.engine.getView().addCamera(str(id), layer, viewport, fife.ExactModelCoordinate(0,0,0))
-		cam.setCellImageDimensions(refw, refh)
-		cam.setRotation(rot)
-		cam.setTilt(tilt)
-	
-		self._widget.hide()
-
-		if self.callback:
-			self.callback()
