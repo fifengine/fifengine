51237f1735c244f468304076b6f95502cfc9a4f0
mvbarracuda <mvbarracuda@web.de>
2009-11-16 12:29:29 +0000

    * Updated win32 build files for the new November 2009 Win32 DevKit  * The old Win32 compile SDK 2008.1-r1 will NOT work with future revisions, starting with 3073  * For details, check this article that also contains the download links: http://fifengine.de/2009/11/16/new-fife-win32-devkit-available/  * Thanks to Cheesesucker for providing the installer version as well as for a bunch of smaller fixes to the build scripts

--- build/win32/update_dlls_mingw.bat
+++ build/win32/update_dlls_mingw.bat
@@ -3,7 +3,5 @@ del .\..\..\clients\editor\*.dll
 del .\..\..\clients\pychan_demo\*.dll
 del .\..\..\clients\rio_de_hola\*.dll
 
-copy .\binaries\mingw\*.dll .\..\..\*.*
-copy .\binaries\mingw\*.dll .\..\..\clients\editor\*.*
-copy .\binaries\mingw\*.dll .\..\..\clients\pychan_demo\*.*
-copy .\binaries\mingw\*.dll .\..\..\clients\rio_de_hola\*.*
+del .\..\..\engine\swigwrappers\python\*.dll
+copy .\binaries\mingw\*.dll .\..\..\engine\swigwrappers\python\*.*
