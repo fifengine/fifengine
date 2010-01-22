35b325ebc46c1464b0bc7f4aba2ea5d9254e7e39
prock <wprasek@gmail.com>
2010-01-22 19:03:43 +0000

    Fixed the scons batch files (win32). Core tests now compile on windows but fail to link.

--- build/win32/build_environments/scons/build_tests.bat
+++ build/win32/build_environments/scons/build_tests.bat
@@ -1,20 +1,8 @@
-:: Users will need to change paths if they use custom MinGW, SCons, Python or SWIG versions that didn't ship with the SDK
-SET _=%CD%
-SET _SWIG="%_%\..\..\applications\swigwin-1.3.40"
+@echo off
 
-:: To avoid path collisions the following line was commented out and replaced with a slightly different version
-:: SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
-SET PATH=%_%\..\..\applications\scons;%_%\..\..\applications\mingw\bin;c:\python25
+:: run scons
+python ..\..\applications\scons\scons.py -C ..\..\..\..\ tests
 
-:: Goto TRUNK and call SCons
-cd \
-cd "%_%\..\..\..\.."
-python build\win32\applications\scons\scons.py tests
+:: copy the needed dlls to the path where fife was installed
+call copy_dlls.bat
 
-:: Delete old versions of _fife.pyd and rename the compiled fife.dll to _fife.pyd
-::cd "%_%\..\..\..\..\engine\swigwrappers\python"
-::del _fife.pyd
-::move "..\..\fife.dll" .\_fife.pyd
-
-:: Return us to the directory we started from
-cd %_%
--- build/win32/build_environments/scons/list_scons_args.bat
+++ build/win32/build_environments/scons/list_scons_args.bat
@@ -1,15 +1,4 @@
-:: Users will need to change paths if they use custom MinGW, SCons, Python or SWIG versions that didn't ship with the SDK
-SET _=%CD%
-SET _SWIG="%_%\..\..\applications\swigwin-1.3.40"
+@echo off
 
-:: To avoid path collisions the following line was commented out and replaced with a slightly different version
-:: SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
-SET PATH=%_%\..\..\applications\scons;%_%\..\..\applications\mingw\bin;c:\python25
-
-:: Goto TRUNK and call SCons
-cd \
-cd "%_%\..\..\..\.."
-python build\win32\applications\scons\scons.py -h > "%_%\scons_args.txt" 2>&1
-
-:: Return us to the directory we started from
-cd %_%
+:: run scons
+python ..\..\applications\scons\scons.py -C ..\..\..\..\ -h > scons_args.txt 2>&1
