223369cb7aa7b0fe4e6f7500aedfcdb54de75a35
vtchill <manning.jesse@gmail.com>
2010-01-23 00:18:54 +0000

    More fixes for the win32 batch files to remove the need to overwrite the users path and define variables. [t:429]

--- build/win32/update_project_files.bat
+++ build/win32/update_project_files.bat
@@ -1,10 +1,4 @@
-:: Users will need to change paths if they use custom MinGW, SCons, Python or SWIG versions that didn't ship with the SDK
-SET _=%CD%
-SET _SWIG="%_%\applications\swigwin-1.3.40"
-
-:: To avoid path collisions the following line was commented out and replaced with a slightly different version
-:: SET PATH=%Path%;%_%\applications\scons;%_%\applications\mingw\bin
-SET PATH=%_%\applications\scons;%_%\applications\mingw\bin;c:\python26
+@echo off
 
 :: Delete old generated project files to ensure that they get generated from scratch again
 cd %_%\build_environments
@@ -12,10 +6,6 @@ del code_blocks\fife.cbp
 del visual_studio_8\fife.vcproj
 del visual_studio_9\fife.vcproj
 
-:: Goto TRUNK and call SCons
-cd \
-cd "%_%\..\..\"
-python build\win32\applications\scons\scons.py projects
+:: run scons
+python applications\scons\scons.py -C ..\..\ projects
 
-:: Return us to the directory we started from
-cd %_%
