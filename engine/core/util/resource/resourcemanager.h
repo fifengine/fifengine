2037b9d9beb33326deec1593950ded7b7fd73bdd
prock <wprasek@gmail.com>
2011-01-18 16:28:17 +0000

    * Commented IResourceManager   * Added some more situations that get logged to the fife log

--- engine/core/util/resource/resourcemanager.h
+++ engine/core/util/resource/resourcemanager.h
@@ -37,70 +37,344 @@
 
 namespace FIFE {
 
+	/** IResourceManager
+	 *
+	 * An interface for managing resources.
+	 *
+	 * @see IResource
+	 * @see IResourceLoader
+	 *
+	 */
+
 	class IResourceManager {
 	public:
+
+		/** Default constructor.
+		 */
 		IResourceManager() { }
+
+		/** Destructor.
+		 */
 		virtual ~IResourceManager();
 
-		//gets the total amount of memory used by resources
+		/** Gets the total amount of memory used by resources
+		 *
+		 * This function uses the IResource's getSize() function
+		 * to calculate the total memory used.  It does not include
+		 * memory used by the internal resource manager book keeping
+		 * structures.  This value is only accurate if the resources
+		 * getSize() function returns an accurate result.
+		 *
+		 * @return Total memory used
+		 *
+		 */
 		virtual size_t getMemoryUsed() const;
 
-		//gets the total number of resources created but not loaded
-		virtual uint32_t getTotalResourcesCreated();
+		/** Returns the number of unloaded resources
+		 *
+		 * Gets the total number of resources created but not loaded.
+		 * These resources have been defined in the resource manager
+		 * but have yet to be loaded from disk.
+		 *
+		 * @return Total resources created
+		 *
+		 */
+		virtual uint32_t getTotalResourcesCreated() const;
 
-		//gets the total number of resource loaded
-		virtual uint32_t getTotalResourcesLoaded();
+		/** Returns the number of loaded resources
+		 *
+		 * Gets the total number of resources created and loaded.
+		 * These resources have been defined in the resource manager
+		 * have been loaded from disk.
+		 *
+		 * @return Total resources loaded
+		 *
+		 */
+		virtual uint32_t getTotalResourcesLoaded() const;
 
-		//gets the total number of resources defined
-		virtual uint32_t getTotalResources();
+		/** Returns the number of defined resources
+		 *
+		 * Gets the total number of resources managed by the manager.
+		 *
+		 * @return Total resources managed
+		 *
+		 */
+		virtual uint32_t getTotalResources() const;
 
-		//creates a blank resource but does not load it immediately
+		/** Creates a blank resource but does not load it immediately
+		 *
+		 * This is a pure virtual function.  The implementor must allocate
+		 * memory for the type of resource the manager will manage.
+		 * Typically from within this function you will call
+		 * IResourceManager::add(IResource* res) to add the resource to
+		 * the manager.
+		 *
+		 * @param name The resource name. Typically a filename.
+		 * @param loader A pointer to the custom resource loader.  The
+		 * default is NULL.  If this parameter is present the resource
+		 * will use the loader to load instead of the default built in
+		 * functions.
+		 * @return A ResourcePtr to the newly created resource
+		 *
+		 * @see IResource
+		 * @see IResourceLoader
+		 *
+		 */
 		virtual ResourcePtr create(const std::string& name, IResourceLoader* loader = 0) = 0;
 
-		//creates a resources and loads it
+		/** Creates a blank resource and loads it from disk
+		 *
+		 * This function will create the resource if necessary and load
+		 * the resource from disk.  If the resource is not defined it
+		 * will call IResrouceManager::create() before loading.
+		 *
+		 * @param name The resource name. Typically a filename.
+		 * @param loader A pointer to the custom resource loader.  The
+		 * default is NULL.  If this parameter is present the resource
+		 * will use the loader to load instead of the default built in
+		 * functions.
+		 * @return A ResourcePtr to the loaded resource
+		 *
+		 * @see IResource
+		 * @see IResourceLoader
+		 *
+		 */
 		virtual ResourcePtr load(const std::string& name, IResourceLoader* loader = 0);
 
-		//add a resrouce to the manager that the manager did not load
+		/** Add a resrouce to the manager
+		 *
+		 * This function will create a ResourcePtr and add the resource
+		 * to the manager.  The manager assumes ownership of the resource
+		 * so DO NOT delete it.
+		 *
+		 * @param res A pointer to the resource
+		 * @return A ResourcePtr to the added resource
+		 *
+		 * @see IResource
+		 *
+		 */
 		virtual ResourcePtr add(IResource* res);
 
-		//checks to make sure resouce exists
+		/** Checks to see if a resource exists
+		 *
+		 * This function will search the manager for resources
+		 * that match the parameter
+		 *
+		 * @param name The name of the resource
+		 * @return True if the resource exists.  False otherwise.
+		 *
+		 */
 		virtual bool exists(const std::string& name);
+
+		/** Checks to see if a resource exists
+		 *
+		 * This function will search the manager for resources
+		 * that match the parameter
+		 *
+		 * @param handle The handle of the resource
+		 * @return True if the resource exists.  False otherwise.
+		 *
+		 */
 		virtual bool exists(ResourceHandle handle);
 
-		//forces a reload of a resource (even if it's not loaded)
+		/** Reloads a resource
+		 *
+		 * This function will reload a resource if it is managed
+		 * by the manager.  If not it creates an entry in the log
+		 * specifying that the resource could not be found.  It will
+		 * load the resource if it is not already loaded.
+		 *
+		 * @param name The name of the resource
+		 *
+		 */
 		virtual void reload(const std::string& name);
-		virtual void reload(ResourceHandle res);
+
+		/** Reloads a resource
+		 *
+		 * This function will reload a resource if it is managed
+		 * by the manager.  If not it creates an entry in the log
+		 * specifying that the resource could not be found.  It will
+		 * load the resource if it is not already loaded.
+		 *
+		 * @param handle The handle of the resource
+		 *
+		 */
+		virtual void reload(ResourceHandle handle);
+
+		/** Reloads all resources
+		 *
+		 * This function will reload all resources managed by the
+		 * manager.  It will load a resource if it is not already loaded.
+		 *
+		 * @param name The name of the resource
+		 *
+		 * @todo It might be beneficial to supply a parameter to
+		 * only reload resources that are already loaded
+		 *
+		 */
 		virtual void reloadAll();
 
-		//loads all unreferenced resources
+		/** Loads all unreferenced resources
+		 *
+		 * All resources that have no external references will be
+		 * loaded into memory.
+		 *
+		 */
 		virtual void loadUnreferenced();
 
-		//free memory but dont remove from manager
+		/** Frees a resource from memory
+		 *
+		 * The resource is not deleted but it's data is freed.
+		 * This calls the IResource::free() function and it is
+		 * up to the resource to properly free it's memory.  The
+		 * manager keeps a reference to the resource in case
+		 * its required in the future.
+		 *
+		 * @param name The name of the resource
+		 *
+		 * @see IResource
+		 *
+		 */
 		virtual void free(const std::string& name);
+
+		/** Frees a resource from memory
+		 *
+		 * The resource is not deleted but it's data is freed.
+		 * This calls the IResource::free() function and it is
+		 * up to the resource to properly free it's memory.  The
+		 * manager keeps a reference to the resource in case
+		 * its required in the future.
+		 *
+		 * @param handle The handle of the resource
+		 *
+		 * @see IResource
+		 *
+		 */
 		virtual void free(ResourceHandle handle);
+
+		/** Frees all resources
+		 *
+		 * This calls the IResource::free() function for every
+		 * resource the manager is managing.  It does not remove
+		 * them from the manager.
+		 *
+		 * @see IResource
+		 *
+		 */
 		virtual void freeAll();
 
-		//all unreferenced resources will be freed but not removed from manager
+		/** Frees all unreferenced resources
+		 *
+		 * This calls the IResource::free() function for resources
+		 * that have no external references to them.  It does not
+		 * remove them from the manager.
+		 *
+		 * @see IResource
+		 *
+		 */
 		virtual void freeUnreferenced();
 
+		/** Removes a resource
+		 *
+		 * This removes all references to the resource from the
+		 * manager. It does not however guarantee that the resources
+		 * destructor is called.  If the client has any left over
+		 * references to the resource it will not be freed.
+		 *
+		 * @param resource A ResourcePtr to the resource to be removed
+		 * from the manager
+		 *
+		 * @note This is useful if you want to remove ownership of a resource
+		 * from the manager
+		 *
+		 * @see IResource
+		 *
+		 */
 		//free memory and remove from manager
 		virtual void remove(ResourcePtr& resource); //useful to remove ownership from the manager
+
+		/** Removes a resource
+		 *
+		 * This removes all references to the resource from the
+		 * manager. It does not however guarantee that the resources
+		 * destructor is called.  If the client has any left over
+		 * references to the resource it will not be freed.
+		 *
+		 * @param name The name of the resource
+		 *
+		 * @see IResource
+		 *
+		 */
 		virtual void remove(const std::string& name);
+
+		/** Removes a resource
+		 *
+		 * This removes all references to the resource from the
+		 * manager. It does not however guarantee that the resources
+		 * destructor is called.  If the client has any left over
+		 * references to the resource it will not be freed.
+		 *
+		 * @param handle The handle of the resource
+		 *
+		 * @see IResource
+		 *
+		 */
 		virtual void remove(ResourceHandle handle);
+
+		/** Removes all resources
+		 *
+		 * This effectively removes all references all resources from
+		 * the manager.  If there are left over shared pointers to
+		 * any resources they will not be deleted.
+		 *
+		 * @see IResource
+		 *
+		 */
 		virtual void removeAll();
 
-		//frees and removes all unreferenced resources
+		/** Removes all unreferenced resources
+		 *
+		 * This effectively removes all resoiurces that dont have an
+		 * external reference.  The resources will be deleted.
+		 *
+		 * @see IResource
+		 *
+		 */
 		virtual void removeUnreferenced();
 
-		//get a shared pointer to the resource
-		//loads the resource if required
+		/** Gets a shared pointer to the resource
+		 *
+		 * If the resource is not defined it will attempt to create
+		 * and load the resource
+		 *
+		 * @param name The name of the resource
+		 * @return A ResourcePtr to the resource
+		 *
+		 */
 		virtual ResourcePtr get(const std::string& name);
+
+		/** Gets a shared pointer to the resource
+		 *
+		 * If the resource is not defined it returns an empty
+		 * (or invalid) SharedPtr and makes an entry in the log.
+		 *
+		 * @param handle The handle of the resource
+		 * @return A ResourcePtr to the resource
+		 *
+		 */
 		virtual ResourcePtr get(ResourceHandle handle);
 
-		//get the resource handle by name
+		/** Gets a resource handle by name
+		 *
+		 * Returns the resource handle associated with the name
+		 *
+		 * @param name The name of the resource
+		 * @return 0 if the resource name is invalid
+		 *
+		 */
 		virtual ResourceHandle getResourceHandle(const std::string& name);
 
-	protected:
+	private:
 		typedef std::map< ResourceHandle, ResourcePtr > ResourceHandleMap;
 		typedef std::map< ResourceHandle, ResourcePtr >::iterator ResourceHandleMapIterator;
 		typedef std::map< ResourceHandle, ResourcePtr >::const_iterator ResourceHandleMapConstIterator;
