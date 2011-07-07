e6c1973f881665f69c8bb60238c220ebbf692b55
kozmo <kozmatic@myopera.com>
2011-07-07 16:50:46 +0000

    * Changed atlas loader to work in similar fashion as animation loader [t:542] * Changed some deprecated boost.filesystem function calls. * Updated license header

--- engine/core/loaders/native/video/atlasloader.cpp
+++ engine/core/loaders/native/video/atlasloader.cpp
@@ -1,23 +1,23 @@
 /***************************************************************************
-*   Copyright (C) 2005-2008 by the FIFE team                              *
-*   http://www.fifengine.de                                               *
-*   This file is part of FIFE.                                            *
-*                                                                         *
-*   FIFE is free software; you can redistribute it and/or                 *
-*   modify it under the terms of the GNU Lesser General Public            *
-*   License as published by the Free Software Foundation; either          *
-*   version 2.1 of the License, or (at your option) any later version.    *
-*                                                                         *
-*   This library is distributed in the hope that it will be useful,       *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
-*   Lesser General Public License for more details.                       *
-*                                                                         *
-*   You should have received a copy of the GNU Lesser General Public      *
-*   License along with this library; if not, write to the                 *
-*   Free Software Foundation, Inc.,                                       *
-*   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA          *
-***************************************************************************/
+ *   Copyright (C) 2005-2011 by the FIFE team                              *
+ *   http://www.fifengine.net                                              *
+ *   This file is part of FIFE.                                            *
+ *                                                                         *
+ *   FIFE is free software; you can redistribute it and/or                 *
+ *   modify it under the terms of the GNU Lesser General Public            *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA          *
+ ***************************************************************************/
 
 // Standard C++ library includes
 
@@ -44,6 +44,39 @@ namespace fs = boost::filesystem;
 namespace FIFE {
 	static Logger _log(LM_NATIVE_LOADERS);
 
+
+	size_t Atlas::getImageCount() const { 
+		return m_subimages.size();
+	}
+
+	ImagePtr& Atlas::getPackedImage() { 
+		return m_image;
+	}
+
+	ImagePtr Atlas::getImage(const std::string& id) {
+		SubimageMap::iterator iter = m_subimages.find(id);
+		if(iter == m_subimages.end())
+			return ImagePtr();
+		return iter->second.image;
+	}
+
+	ImagePtr Atlas::getImage(uint32_t index) {
+		if(index > getImageCount())
+			return ImagePtr();
+
+		SubimageMap::iterator iter = m_subimages.begin();
+		for(uint32_t i = 0; i < index; ++i, ++iter);
+		return iter->second.image;
+	}
+
+	bool Atlas::addImage(const std::string& imagename, const AtlasData& data) {
+		return m_subimages.insert(std::pair<std::string, AtlasData>(imagename, data)).second;
+	}
+
+	void Atlas::setPackedImage(const ImagePtr& image) {
+		m_image = image;
+	}
+
 	AtlasLoader::AtlasLoader(VFS* vfs, ImageManager* imageManager)
 	: m_vfs(vfs), m_imageManager(imageManager) {
 	}
@@ -53,7 +86,7 @@ namespace FIFE {
 
 	bool AtlasLoader::isLoadable(const std::string& filename) {
 		fs::path atlasPath(filename);
-		std::string atlasFilename = atlasPath.file_string();
+		std::string atlasFilename = atlasPath.string();
 
 		try {
 			RawData* data = m_vfs->open(atlasFilename);
@@ -82,7 +115,7 @@ namespace FIFE {
 
 	Atlas* AtlasLoader::load(const std::string& filename) {
 		fs::path atlasPath(filename);
-		std::string atlasFilename = atlasPath.file_string();
+		std::string atlasFilename = atlasPath.string();
 
 		TiXmlDocument doc;
 		Atlas* atlas = NULL;
@@ -121,25 +154,24 @@ namespace FIFE {
 		if (root) {
 			const std::string* atlasName = root->Attribute(std::string("name"));
 			if(atlasName) {
-				atlas = new Atlas(*atlasName);
+				fs::path atlasImagePath = atlasPath.parent_path() / *atlasName;
+				atlas = new Atlas(atlasImagePath.string());
 
 				// End-user could create the same atlas for the second time.
 				// Since we don't hold any data for Atlases like ImageManager we need to recreate 
 				// atlas parameters (to return proper AtlasPtr) but don't reload pixel data (they are held by ImageManager).
 
-				bool atlasExists = m_imageManager->exists(*atlasName);
+				bool atlasExists = m_imageManager->exists(atlas->getName());
 
 				if(!atlasExists) {
-					atlas->setImage(m_imageManager->load(*atlasName));
-					atlas->getImage()->forceLoadInternal();
+					atlas->setPackedImage(m_imageManager->load(atlas->getName()));
+					atlas->getPackedImage()->forceLoadInternal();
 				}
 
 				// We don't really need this now, though we could use it to assert if the loaded atlas is the same sized as these
 				//int atlasWidth, atlasHeight;
 				//root->QueryValueAttribute("width", &atlasWidth);
 				//root->QueryValueAttribute("height", &atlasHeight);
-
-				Atlas::SubimageMap& subimages = atlas->getSubimages();
 
 				for (TiXmlElement* imageElem = root->FirstChildElement("image");
 					imageElem != 0; imageElem = imageElem->NextSiblingElement("image")) {
@@ -151,11 +183,22 @@ namespace FIFE {
 					imageElem->QueryValueAttribute("height", &region.h);
 
 					const std::string* subimageName = imageElem->Attribute(std::string("source"));
-					subimages.insert(std::make_pair(*subimageName, region));
 
-					if(!atlasExists) {
-						ImagePtr subImage = m_imageManager->create(*subimageName);
-						subImage->useSharedImage(atlas->getImage(), region);
+					if(!atlasExists && subimageName) {
+						fs::path subimagePath(filename);
+
+						if(subimagePath.has_parent_path()) {
+							subimagePath = subimagePath.parent_path() / *subimageName;
+						} else {
+							subimagePath = fs::path(*subimageName);
+						}
+
+						std::string finalname = subimagePath.string();
+						ImagePtr subImage = m_imageManager->create(finalname);
+						subImage->useSharedImage(atlas->getPackedImage(), region);
+						
+						Atlas::AtlasData atlas_data = {region, subImage};
+						atlas->addImage(finalname, atlas_data);
 					}
 				}
 			}
