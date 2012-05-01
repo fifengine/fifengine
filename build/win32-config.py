0b96b5420a660316659179fdd48d8d3b067bdc01
prock <wprasek@gmail.com>
2012-05-01 16:47:47 +0000

    * Updated the header file location for fifechan when building with mingw on windows.  [t:698]

--- build/win32-config.py
+++ build/win32-config.py
@@ -39,7 +39,7 @@ def initEnvironment(env):
 	includepath = os.path.join(path, 'build', 'win32', 'includes')
 	staticlibpath = os.path.join(path, 'build', 'win32', 'static_libs', 'mingw')
 
-	env.Append(CPPPATH = [includepath + '\\libogg', includepath + '\\openal', includepath + '\\sdl_image', includepath + '\\zlib', includepath + '\\libguichan', includepath + '\\boost_1_47_0', includepath + '\\libvorbis', includepath + '\\libpng', includepath + '\\sdl_ttf', includepath + '\\sdl', includepath + '\\python27', includepath + '\\unittest++\\src'])
+	env.Append(CPPPATH = [includepath + '\\libogg', includepath + '\\openal', includepath + '\\sdl_image', includepath + '\\zlib', includepath + '\\libfifechan', includepath + '\\boost_1_47_0', includepath + '\\libvorbis', includepath + '\\libpng', includepath + '\\sdl_ttf', includepath + '\\sdl', includepath + '\\python27', includepath + '\\unittest++\\src'])
 	env.Append(LIBPATH = [staticlibpath, staticlibpath + '\\python27'])
 	
 	env.Tool('swig')
