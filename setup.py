f556fb5f1282d85f11a7aaf1c708cb5f9f7c2471
prock <wprasek@gmail.com>
2013-02-15 21:10:48 +0000

    * Updated version numbers and dates.  Final prep before 0.3.4rc1 release.  [t:601]

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ else:
 
 
 setup(name='fife',
-      version='0.3.3r3',
+      version='0.3.4rc1',
       description='Flexible Isometric Free Engine',
       url='www.fifengine.net',
       packages = ['fife', 'fife.extensions', 'fife.extensions.pychan', 'fife.extensions.pychan.widgets', 'fife.extensions.pychan.widgets.ext', 'fife.extensions.serializers' ],
