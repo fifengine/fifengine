fad73bb1da0d9b562cc0cbadd524092836a29819
Connor Shearwood <drake@lifein2d.co.uk>
2013-04-11 21:26:54 +0100

    Cleaned up tools\fontdefupgrader.py
    Added a message to tools\fontdefupgrader.py that gets printed to the console if the script is called without arguments instructing the user on the tool's use. Shifted main script logic to a function and added a docstring. File is now safe to be imported as a python module.

--- tools/fontdefupgrader.py
+++ tools/fontdefupgrader.py
@@ -31,32 +31,59 @@ def _get(conf, section, part, default=None):
 		return default
 	return conf.get(section, part)
 
+def convertFontdefToXML(files, printToConsole=False):
+	"""
+	Converts a list of files containing old pychan font definitions to the new xml format
 
-for filename in argv[1:]:
-	print "Converting", filename
-	xmldef = "<fonts>\n"
-	fontdef = ConfigParser()
-	fontdef.read(filename)
+	@param files: a list of file names to be converted
+	@param printToConsole: a boolean controlling if the function prints status messages to the console
+	@return: None
+	"""
+	
+	if printToConsole: print "Received", len(files), "files to convert"
+	for filename in files:
+		if printToConsole: print "Converting", filename
+		xmldef = "<fonts>\n"
+		fontdef = ConfigParser()
+		fontdef.read(filename)
 
-	def get(section, part, default=""):
-		return _get(fontdef, section, part, default)
+		def get(section, part, default=""):
+			return _get(fontdef, section, part, default)
 
-	sections = [section for section in fontdef.sections() if section.startswith("Font/")]
+		sections = [section for section in fontdef.sections() if section.startswith("Font/")]
 
-	for section in sections:
-		print "Converting:", section[5:]
-		xmldef += '\t<font name=\"' + section[5:] + '"'
-		xmldef += ' type="' + get(section, "type") + '"'
-		xmldef += ' source="' + get(section, "source") + '"'
-		xmldef += ' row_spacing="' + get(section, "row_spacing", "0") + '"'
-		xmldef += ' glyph_spacing="' + get(section, "glyph_spacing", "0") + '"'
-		if get(section, "type") == "truetype":
-			xmldef += ' size="' + get(section, "size") + '"'
-			xmldef += ' antialias="' + get(section, "antialias", "1") + '"'
-			xmldef += ' color="' + get(section, "color", "255,255,255") + '"'
-		xmldef += "/>\n"
+		for section in sections:
+			if printToConsole: print "Parsing", section[5:]
+			xmldef += '\t<font name=\"' + section[5:] + '"'
+			xmldef += ' type="' + get(section, "type") + '"'
+			xmldef += ' source="' + get(section, "source") + '"'
+			xmldef += ' row_spacing="' + get(section, "row_spacing", "0") + '"'
+			xmldef += ' glyph_spacing="' + get(section, "glyph_spacing", "0") + '"'
+			if get(section, "type") == "truetype":
+				xmldef += ' size="' + get(section, "size") + '"'
+				xmldef += ' antialias="' + get(section, "antialias", "1") + '"'
+				xmldef += ' color="' + get(section, "color", "255,255,255") + '"'
+			xmldef += "/>\n"
 
-	xmldef += "</fonts>\n"
+		xmldef += "</fonts>\n"
 
-	with open(filename[:-7] + "xml", "w") as f:
-		f.write(xmldef)
+		with open(filename[:-7] + "xml", "w") as f:
+			if printToConsole: print "Saving", filename[:-7] + "xml"
+			f.write(xmldef)
+
+if __name__=="__main__":
+	print "FIFE .fontdef to XML file converter"
+	print "==================================="
+	print
+	if len(argv[1:]) > 0:
+		convertFontdefToXML(argv[1:], printToConsole=True)
+	else:
+		print "Converts old .fontdef files to the new xml file format"
+		print "To convert, run the following:"
+		print "\tpython", argv[0], "[filenames]"
+		print "with the filenames separated by spaces"
+		print "It assumes that all input files end with '.fontdef'"
+		print "and creates new files with an '.xml' extension. So"
+		print "the input file 'foo.fontdef' would create a new output"
+		print "'foo.xml' in the same folder. The old fontdef files are"
+		print "left untouched."
