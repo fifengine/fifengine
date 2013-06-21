37513121ec20f3198b5fea21b263dd4b00e088db
prock <wprasek@gmail.com>
2013-06-21 10:49:08 -0500

    Undid some of my last commit but still fixed the Icon constructor
    I didn't realize that clients were depending on the icon to take the size
    of the image.  I've undid my last commit but added a fix that fixes the
    icons constructor to set the icon size as expected.
    
    refs #751

--- engine/python/fife/extensions/pychan/widgets/icon.py
+++ engine/python/fife/extensions/pychan/widgets/icon.py
@@ -83,6 +83,10 @@ class Icon(Widget):
 								  is_focusable=is_focusable,
 								  comment=comment)
 		self.image = image
+		
+		#if the size parameter is specified set it (again) to override
+		#the icons size.
+		if size is not None: self.size = size
 
 	def clone(self, prefix):
 		iconClone = Icon(None,
@@ -112,9 +116,7 @@ class Icon(Widget):
 	_image = ImageProperty("Image")
 
 	def _setImage(self,source):
-		size = self.size
 		self._image = source
-		self.size = size
 
 	def _getImage(self):
 		return self._image
