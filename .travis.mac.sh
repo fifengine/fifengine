0d4e0753688d2aeef038e59a9ea0468b62f5a5a8
Thomas Kinnen <thomas.kinnen@gmail.com>
2014-02-02 23:56:02 +0100

    Fix stupid mistake in build file

--- .travis.mac.sh
+++ .travis.mac.sh
@@ -43,7 +43,7 @@ function install_fifechan() {
     # https://github.com/fifengine/fifechan/blob/master/INSTALL.md
     cd fifechan
     cmake .
-    make | exit 1
+    make || exit 1
     sudo make install
     cd ..
 
@@ -53,16 +53,16 @@ function install_fifechan() {
 
 function build() {
     # https://github.com/fifengine/fifengine/wiki/scons
-    scons | exit 1
+    scons || exit 1
 }
 
 function install() {
-    install_dependencies | exit 1
-    install_fifechan | exit 1
-    build | exit 1
+    install_dependencies || exit 1
+    install_fifechan || exit 1
+    build || exit 1
 }
 
-install | exit 1
+install || exit 1
 
 ## Success
 echo
