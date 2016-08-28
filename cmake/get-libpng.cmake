a92aec93b488471b3da9a5958404a4a9965daf66
jakoch <jakoch@web.de>
2016-08-28 18:37:40 +0200

    fix building libpng fix finding zlib dependency

--- cmake/get-libpng.cmake
+++ cmake/get-libpng.cmake
@@ -9,29 +9,10 @@ include(ExternalProject)
 
 set(LIBPNG_VERSION "1.6.24")
 
-# For Windows it's faster to download the pre-compiled development binaries.
-if (WIN32)
-
-  if(MSVC) 
-
-    ExternalProject_Add(
-      libpng
-      DEPENDS         zlib
-      PREFIX          ${DEPENDENCY_EXTRACT_DIR}  
-      DOWNLOAD_DIR    ${DEPENDENCY_DOWNLOAD_DIR}
-      URL             ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/lpng1624.zip
-      URL_MD5         9572a426a87407f6cfa41f61cbe2e3b2
-      BUILD_COMMAND   "msbuild ${DEPENDENCY_EXTRACT_DIR}/src/libpng/projects/vstudio/vstudio.sln /target:libpng /p:Configuration=Release /p:Platform=Win32 /v:minimal"
-      INSTALL_COMMAND
-        COMMAND ${CMAKE_COMMAND} -E copy "${DEPENDENCY_EXTRACT_DIR}/src/libpng/projects/vstudio/Release Library/libpng16.lib" "${DEPENDENCY_INSTALL_DIR}/lib/libpng16.lib"    
-    )
-  
-  endif(MSVC)
-
-else() # build from source
-
-  set(LIBPNG_CMAKE_ARGS
+set(LIBPNG_CMAKE_ARGS
       ${COMMON_CMAKE_ARGS}
+      -DZLIB_LIBRARY=${DEPENDENCY_INSTALL_DIR}/lib
+      -DZLIB_INCLUDE_DIR=${DEPENDENCY_INSTALL_DIR}/include
       -DPNG_SHARED=off
       -DPNG_STATIC=on
       -DPNG_TESTS=off
@@ -45,8 +26,31 @@ else() # build from source
       -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
       -DCMAKE_C_FLAGS=${GL_BINDING_C_FLAGS}
       -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-  )
+)
 
+# For Windows it's faster to download the pre-compiled development binaries.
+if (WIN32)
+
+  if(MSVC)
+
+    ExternalProject_Add(
+      libpng
+      DEPENDS         zlib
+      PREFIX          ${DEPENDENCY_EXTRACT_DIR}  
+      DOWNLOAD_DIR    ${DEPENDENCY_DOWNLOAD_DIR}
+      URL             ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/lpng1624.zip
+      URL_MD5         9572a426a87407f6cfa41f61cbe2e3b2  
+      #BUILD_COMMAND   "msbuild ${DEPENDENCY_EXTRACT_DIR}/src/libpng/projects/vstudio/vstudio.sln /t:libpng /p:Configuration=Release /p:Platform=Win32 /v:minimal"
+      INSTALL_COMMAND
+        COMMAND ${CMAKE_COMMAND} -E copy "${DEPENDENCY_EXTRACT_DIR}/src/libpng/projects/vstudio/Release Library/libpng16.lib" "${DEPENDENCY_INSTALL_DIR}/lib/libpng16.lib" 
+      INSTALL_DIR     ${DEPENDENCY_INSTALL_DIR}
+      CMAKE_ARGS      ${LIBPNG_CMAKE_ARGS}     
+    )
+
+  endif(MSVC)
+
+else() # build from source
+ 
   ExternalProject_Add(
     libpng
     DEPENDS      zlib
