759680b2ee6f9c3e62754dcec6a0bceb06019d44
jakoch <jakoch@web.de>
2016-08-28 12:12:36 +0200

    add get-zlib and provide guidance to find stuff by altering prefix path

---
+++ cmake/get-zlib.cmake
@@ -0,0 +1,36 @@
+#------------------------------------------------------------------------------
+# External Project:     zlib
+# Downloads:            http://zlib.net/
+# Github:               https://github.com/madler/zlib                                       
+#------------------------------------------------------------------------------
+
+include(ExternalProject)
+
+set(ZLIB_VERSION "1.2.8")
+
+set(ZLIB_CMAKE_ARGS
+   ${COMMON_CMAKE_ARGS}
+    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    -DCMAKE_INSTALL_PREFIX=${DEPENDENCY_INSTALL_DIR}
+    #-DCMAKE_PREFIX_PATH=${DEPENDENCY_INSTALL_DIR} ${CMAKE_PREFIX_PATH}
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+    -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+)
+
+ExternalProject_Add(
+    zlib
+    PREFIX ${DEPENDENCY_EXTRACT_DIR}
+    DOWNLOAD_DIR ${DEPENDENCY_DOWNLOAD_DIR}
+    URL https://github.com/madler/zlib/archive/v${ZLIB_VERSION}.zip    
+    #URL_MD5
+    INSTALL_DIR ${DEPENDENCY_INSTALL_DIR}
+    CMAKE_ARGS ${ZLIB_CMAKE_ARGS}
+)
+
+# rename "downloads/v1.2.3.zip" to "downloads/zlib-v1.2.3.zip"
+ExternalProject_Add_Step(
+    zlib 
+    RenameDownloadFile
+    COMMAND ${CMAKE_COMMAND} -E rename "${DEPENDENCY_DOWNLOAD_DIR}/v${ZLIB_VERSION}.zip" "${DEPENDENCY_DOWNLOAD_DIR}/zlib-v${ZLIB_VERSION}.zip"
+    DEPENDEES install
+)
