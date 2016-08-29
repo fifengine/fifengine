8be45456c9fd957a26ae0178578ec4025f3c3494
jakoch <jakoch@web.de>
2016-08-29 16:53:06 +0200

    cleanup

--- cmake/get-libpng.cmake
+++ cmake/get-libpng.cmake
@@ -10,52 +10,31 @@ include(ExternalProject)
 set(LIBPNG_VERSION "1.6.24")
 
 set(LIBPNG_CMAKE_ARGS
-      ${COMMON_CMAKE_ARGS}
-      -DZLIB_LIBRARY:PATH=${DEPENDENCY_INSTALL_DIR}/lib
-      -DZLIB_INCLUDE_DIR:PATH=${DEPENDENCY_INSTALL_DIR}/include
-      -DPNG_SHARED=off
+    ${COMMON_CMAKE_ARGS}
+      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+      -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+      -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+      -DBUILD_SHARED_LIBS=TRUE
+      -DPNG_SHARED=on
       -DPNG_STATIC=on
       -DPNG_TESTS=off
-      -DPNG_DEBUG=off
-      -DPNGARG=off
-      -DBUILD_SHARED_LIBS=FALSE
-      -DCMAKE_INSTALL_PREFIX:PATH=${DEPENDENCY_INSTALL_DIR}
-      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-      -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+      -DZLIB_LIBRARY:PATH=${DEPENDENCY_INSTALL_DIR}/lib
+      -DZLIB_INCLUDE_DIR:PATH=${DEPENDENCY_INSTALL_DIR}/include    
 )
     
-# For Windows it's faster to download the pre-compiled development binaries.
-if (WIN32)
+#BUILD_COMMAND   "msbuild ${DEPENDENCY_EXTRACT_DIR}/src/libpng/projects/vstudio/vstudio.sln /t:libpng /p:Configuration=Release /p:Platform=Win32 /v:minimal"
+#INSTALL_COMMAND
+#  COMMAND ${CMAKE_COMMAND} -E copy "${DEPENDENCY_EXTRACT_DIR}/src/libpng/projects/vstudio/Release Library/libpng16.lib" "${DEPENDENCY_INSTALL_DIR}/lib/libpng16.lib" 
 
-  if(MSVC)
-
-    ExternalProject_Add(
-      libpng
-      DEPENDS         zlib
-      PREFIX          ${DEPENDENCY_EXTRACT_DIR}  
-      DOWNLOAD_DIR    ${DEPENDENCY_DOWNLOAD_DIR}
-      URL             ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/lpng1624.zip
-      URL_MD5         9572a426a87407f6cfa41f61cbe2e3b2  
-      #BUILD_COMMAND   "msbuild ${DEPENDENCY_EXTRACT_DIR}/src/libpng/projects/vstudio/vstudio.sln /t:libpng /p:Configuration=Release /p:Platform=Win32 /v:minimal"
-      INSTALL_COMMAND
-        COMMAND ${CMAKE_COMMAND} -E copy "${DEPENDENCY_EXTRACT_DIR}/src/libpng/projects/vstudio/Release Library/libpng16.lib" "${DEPENDENCY_INSTALL_DIR}/lib/libpng16.lib" 
-      INSTALL_DIR     ${DEPENDENCY_INSTALL_DIR}
-      CMAKE_ARGS      ${LIBPNG_CMAKE_ARGS}     
-    )
-
-  endif(MSVC)
-
-else() # build from source
-
-  ExternalProject_Add(
-    libpng
-    DEPENDS      zlib
-    PREFIX       ${DEPENDENCY_EXTRACT_DIR}  
-    DOWNLOAD_DIR ${DEPENDENCY_DOWNLOAD_DIR}
-    URL          ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/lpng1624.zip
-    URL_MD5      9572a426a87407f6cfa41f61cbe2e3b2
-    INSTALL_DIR  ${DEPENDENCY_INSTALL_DIR}
-    CMAKE_ARGS   ${LIBPNG_CMAKE_ARGS}     
-  )
-
-endif()
+ExternalProject_Add(
+  libpng
+  DEPENDS      zlib
+  PREFIX       ${DEPENDENCY_EXTRACT_DIR}  
+  DOWNLOAD_DIR ${DEPENDENCY_DOWNLOAD_DIR}
+  URL          ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/lpng1624.zip
+  URL_MD5      9572a426a87407f6cfa41f61cbe2e3b2
+  INSTALL_DIR  ${DEPENDENCY_INSTALL_DIR}
+  CMAKE_ARGS   ${LIBPNG_CMAKE_ARGS}     
+)
