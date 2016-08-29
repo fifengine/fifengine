8c2cd4911404ea28be5673c398c88ba60dfe6a5b
jakoch <jakoch@web.de>
2016-08-29 18:17:55 +0200

    try a custom configure_command and using devenv

--- cmake/get-libpng.cmake
+++ cmake/get-libpng.cmake
@@ -13,6 +13,7 @@ set(LIBPNG_CMAKE_ARGS
     ${COMMON_CMAKE_ARGS}
       -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
       -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+      -DCMAKE_CONFIGURATION_TYPES=Release
       -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
       -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
       -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
@@ -30,11 +31,18 @@ set(LIBPNG_CMAKE_ARGS
 
 ExternalProject_Add(
   libpng
-  DEPENDS      zlib
-  PREFIX       ${DEPENDENCY_EXTRACT_DIR}  
-  DOWNLOAD_DIR ${DEPENDENCY_DOWNLOAD_DIR}
-  URL          ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/lpng1624.zip
-  URL_MD5      9572a426a87407f6cfa41f61cbe2e3b2
-  INSTALL_DIR  ${DEPENDENCY_INSTALL_DIR}
-  CMAKE_ARGS   ${LIBPNG_CMAKE_ARGS}     
+  DEPENDS             zlib
+  PREFIX              ${DEPENDENCY_EXTRACT_DIR}  
+  DOWNLOAD_DIR        ${DEPENDENCY_DOWNLOAD_DIR}
+  URL                 ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/lpng1624.zip
+  URL_MD5             9572a426a87407f6cfa41f61cbe2e3b2
+  INSTALL_DIR         ${DEPENDENCY_INSTALL_DIR}
+  CONFIGURE_COMMAND   ${CMAKE_COMMAND} ${DEPENDENCY_EXTRACT_DIR}/src/libpng 
+         -G ${CMAKE_GENERATOR} 
+         -DZLIB_INCLUDE_DIR:PATH=${DEPENDENCY_INSTALL_DIR}/zlib/include
+         -DZLIB_LIBRARY:FILEPATH=${DEPENDENCY_INSTALL_DIR}/zlib/lib/zlib.lib
+         -DCMAKE_INSTALL_PREFIX=${DEPENDENCY_INSTALL_DIR}
+  BUILD_COMMAND       devenv libpng.sln /build Release /project png16
+  INSTALL_COMMAND     devenv libpng.sln /build Release /project INSTALL
+  #CMAKE_ARGS   ${LIBPNG_CMAKE_ARGS}     
 )
