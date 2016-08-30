e220a544f211824096d71cd5c5b4fb7d139f5a5e
jakoch <jakoch@web.de>
2016-08-30 02:09:54 +0200

    switch libpng download source, the old one was incomplete adjust build command

--- cmake/get-libpng.cmake
+++ cmake/get-libpng.cmake
@@ -29,15 +29,16 @@ ExternalProject_Add(
   DEPENDS             zlib
   PREFIX              ${DEPENDENCY_EXTRACT_DIR}  
   DOWNLOAD_DIR        ${DEPENDENCY_DOWNLOAD_DIR}
-  URL                 ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/lpng1624.zip
-  URL_MD5             9572a426a87407f6cfa41f61cbe2e3b2
+  URL                 https://github.com/glennrp/libpng/archive/v1.6.24.zip
+  URL_MD5             8e8d519124c7c6a2b7ec72a23c453fbd
   INSTALL_DIR         ${DEPENDENCY_INSTALL_DIR}
   CONFIGURE_COMMAND   ${CMAKE_COMMAND} ${DEPENDENCY_EXTRACT_DIR}/src/libpng 
          -G ${CMAKE_GENERATOR} 
          -DZLIB_INCLUDE_DIR:PATH=${DEPENDENCY_INSTALL_DIR}/zlib/include
          -DZLIB_LIBRARY:FILEPATH=${DEPENDENCY_INSTALL_DIR}/zlib/lib/zlib.lib
          -DCMAKE_INSTALL_PREFIX=${DEPENDENCY_INSTALL_DIR}
-  BUILD_COMMAND       msbuild libpng.sln /build Release /project png16
-  INSTALL_COMMAND     msbuild libpng.sln /build Release /project INSTALL
+  BUILD_COMMAND       ${CMAKE_COMMAND} --build ${DEPENDENCY_EXTRACT_DIR}/src/libpng-build --config Release --target INSTALL
+  #BUILD_COMMAND       msbuild libpng.sln /target:INSTALL /p:Platform=Win32 /p:Configuration=Release
+  #INSTALL_COMMAND     msbuild libpng.sln /build Release /project INSTALL
   #CMAKE_ARGS   ${LIBPNG_CMAKE_ARGS}     
 )
