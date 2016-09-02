52a47e164c15c8da51273c05da0ca74ba64d5208
jakoch <jakoch@web.de>
2016-09-02 15:26:47 +0200

    install command for tinyxml

--- cmake/get-tinyxml.cmake
+++ cmake/get-tinyxml.cmake
@@ -16,10 +16,10 @@ ExternalProject_Add(
   URL                   https://sourceforge.net/projects/tinyxml/files/tinyxml/2.6.2/tinyxml_2_6_2.zip/download  
   URL_MD5               2a0aaf609c9e670ec9748cd01ed52dae
   PATCH_COMMAND         ${CMAKE_COMMAND} -E copy ${CMAKE_MODULE_PATH}/patches/tinyxml/CMakeLists.txt ${DEPENDENCY_EXTRACT_DIR}/src/tinyxml
-  INSTALL_COMMAND 
-    COMMAND             ${CMAKE_COMMAND} -E copy ${DEPENDENCY_EXTRACT_DIR}/src/tinyxml-build/Release/tinyxml.dll   ${DEPENDENCY_INSTALL_DIR}/bin/tinyxml.dll    
-    COMMAND             ${CMAKE_COMMAND} -E copy ${DEPENDENCY_EXTRACT_DIR}/src/tinyxml-build/Release/tinyxml.lib   ${DEPENDENCY_INSTALL_DIR}/lib/tinyxml.lib    
-    COMMAND             ${CMAKE_COMMAND} -E copy ${DEPENDENCY_EXTRACT_DIR}/src/tinyxml/tinystr.h"                  ${DEPENDENCY_INSTALL_DIR}/include/tinystr.h
-    COMMAND             ${CMAKE_COMMAND} -E copy ${DEPENDENCY_EXTRACT_DIR}/src/tinyxml/tinyxml.h"                  ${DEPENDENCY_INSTALL_DIR}/include/tinyxml.h
+  INSTALL_COMMAND       ${CMAKE_COMMAND} -E make_directory ${DEPENDENCY_INSTALL_DIR}/include/tinyxml
+    COMMAND ${CMAKE_COMMAND} -E copy ${DEPENDENCY_EXTRACT_DIR}/src/tinyxml-build/Release/tinyxml.dll  ${DEPENDENCY_INSTALL_DIR}/bin/tinyxml.dll    
+    COMMAND ${CMAKE_COMMAND} -E copy ${DEPENDENCY_EXTRACT_DIR}/src/tinyxml-build/Release/tinyxml.lib  ${DEPENDENCY_INSTALL_DIR}/lib/tinyxml.lib    
+    COMMAND ${CMAKE_COMMAND} -E copy ${DEPENDENCY_EXTRACT_DIR}/src/tinyxml/tinystr.h"                 ${DEPENDENCY_INSTALL_DIR}/include/tinyxml/tinystr.h
+    COMMAND ${CMAKE_COMMAND} -E copy ${DEPENDENCY_EXTRACT_DIR}/src/tinyxml/tinyxml.h"                 ${DEPENDENCY_INSTALL_DIR}/include/tinyxml/tinyxml.h
   
 )
