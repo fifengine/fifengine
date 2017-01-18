a025e1f57b8adc2a94b35646877d4db1041b60a1
helios2000 <helios2000@gmx.net>
2017-01-18 17:22:40 +0100

    * Changed tinyxml include in animationmanager.cpp

--- engine/core/video/animationmanager.cpp
+++ engine/core/video/animationmanager.cpp
@@ -23,16 +23,15 @@
 #include <map>
 
 // 3rd party library includes
+#include <tinyxml.h>
 
 // FIFE includes
 // These includes are split up in two parts, separated by one empty line
 // First block: files included from the FIFE root src directory
 // Second block: files included from the same folder
-#include "ext/tinyxml/fife_tinyxml.h"
 #include "util/log/logger.h"
 #include "util/resource/resourcemanager.h"
 #include "util/resource/resource.h"
-//#include "video/image.h"
 #include "video/renderbackend.h"
 
 #include "animationmanager.h"
