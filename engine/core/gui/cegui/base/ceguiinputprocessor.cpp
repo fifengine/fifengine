34690a1674796fb86d1a179d59e92082ce94cc75
Karsten Bock <KarstenBock@gmx.net>
2017-01-23 12:51:35 +0100

    Fixes "jump to case label crosses initialization of" error in GCC.

--- engine/core/gui/cegui/base/ceguiinputprocessor.cpp
+++ engine/core/gui/cegui/base/ceguiinputprocessor.cpp
@@ -103,6 +103,7 @@ namespace FIFE {
 
 		switch(event.type) {
 			case SDL_MOUSEBUTTONDOWN:
+            {
 				switch(event.button.button) {
 					case SDL_BUTTON_LEFT:
 						consumed = CEGUI::System::getSingleton().getDefaultGUIContext().injectMouseButtonDown(CEGUI::LeftButton);
@@ -120,8 +121,10 @@ namespace FIFE {
 						;
 				}
 				break;
+            }
 				
 			case SDL_MOUSEBUTTONUP:
+            {
 				switch(event.button.button) {
 					case SDL_BUTTON_LEFT:
 						consumed = CEGUI::System::getSingleton().getDefaultGUIContext().injectMouseButtonUp(CEGUI::LeftButton);
@@ -139,8 +142,10 @@ namespace FIFE {
 						;
 				}
 				break;
+            }
 			
 			case SDL_MOUSEWHEEL:
+            {
 				// mousewheel up or down
 				int32_t wheelChange = event.wheel.y;
 				if (wheelChange != 0) {
@@ -150,6 +155,7 @@ namespace FIFE {
 					consumed = CEGUI::System::getSingleton().getDefaultGUIContext().injectMouseWheelChange(wheelChange);
 				}
 				break;
+            }
 
 			default:
 				;
