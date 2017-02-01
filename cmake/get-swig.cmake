f8091f189ffd577d03698b96c564941307d51b72
Thomas Kowaliczek <linuxdonald@posteo.de>
2017-02-01 17:16:53 +0100

    forget to change swig md5sum to the new one.

--- cmake/get-swig.cmake
+++ cmake/get-swig.cmake
@@ -17,7 +17,7 @@ if(WIN32)
         PREFIX              ${DEPENDENCY_EXTRACT_DIR}
         DOWNLOAD_DIR        ${DEPENDENCY_DOWNLOAD_DIR}
         URL                 http://downloads.sourceforge.net/project/swig/swigwin/swigwin-${SWIG_VERSION}/swigwin-${SWIG_VERSION}.zip
-        URL_MD5             5cc57e3658f62b7f648bec7eed91a4f0
+        URL_MD5             a49524dad2c91ae1920974e7062bfc93
         #INSTALL_DIR         ${DEPENDENCY_INSTALL_DIR}/../build-tools/swig
         CONFIGURE_COMMAND   ""
         BUILD_COMMAND       ""
