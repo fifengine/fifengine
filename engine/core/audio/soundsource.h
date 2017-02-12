c3a9045dd8eab5479176ac9574473b1c4fcd8902
helios2000 <helios2000@gmx.net>
2017-02-12 16:47:57 +0100

    Added SoundSource class. Refs #485 That is the interface between Instance and SoundEmitter.

---
+++ engine/core/audio/soundsource.h
@@ -0,0 +1,69 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2017 by the FIFE team                              *
+ *   http://www.fifengine.net                                              *
+ *   This file is part of FIFE.                                            *
+ *                                                                         *
+ *   FIFE is free software; you can redistribute it and/or                 *
+ *   modify it under the terms of the GNU Lesser General Public            *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA          *
+ ***************************************************************************/
+
+#ifndef FIFE_SOUNDSOURCE_H
+#define FIFE_SOUNDSOURCE_H
+
+// Standard C++ library includes
+
+// Platform specific includes
+
+// 3rd party library includes
+
+// FIFE includes
+// These includes are split up in two parts, separated by one empty line
+// First block: files included from the FIFE root src directory
+// Second block: files included from the same folder
+
+namespace FIFE {
+
+	class ActionAudio;
+	class Instance;
+	class SoundChangeListener;
+	class SoundEmitter;
+
+	/** Interface class between Instance / ActionAudio and SoundEmitter.
+	 */
+	class SoundSource {
+	public:
+
+		SoundSource(Instance* instance);
+		~SoundSource();
+
+		void setActionAudio(ActionAudio* audio);
+		ActionAudio* getActionAudio() const;
+		void setPosition();
+		void setDirection();
+
+	
+	private:
+		void updateSoundEmitter();
+
+		Instance* m_instance;
+		ActionAudio* m_audio;
+		SoundEmitter* m_emitter;
+		SoundChangeListener* m_listener;
+
+
+	};
+}
+
+#endif
