b4bffa964be6b7bcf3e7c3a1a0d322a279dd04f9
helios2000 <helios2000@gmx.net>
2017-03-01 19:52:33 +0100

    Try to fix travis problem with uint and int types.

--- engine/core/audio/soundconfig.h
+++ engine/core/audio/soundconfig.h
@@ -30,6 +30,7 @@
 // These includes are split up in two parts, separated by one empty line
 // First block: files included from the FIFE root src directory
 // Second block: files included from the same folder
+#include "util/base/fife_stdint.h"
 
 namespace FIFE {
 
