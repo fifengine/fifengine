27dda41cd785b452fd69f372a269da806bea8dd6
Karsten Bock <KarstenBock@gmx.net>
2018-01-04 15:12:18 +0100

    Use boost 1.65.1

--- build/appveyor/copy-boost-to-includes.bat
+++ build/appveyor/copy-boost-to-includes.bat
@@ -7,17 +7,17 @@ mkdir C:\projects\fifengine-dependencies\includes\lib\boost
 mkdir C:\projects\fifengine-dependencies\includes\include\boost
 
 :: copy boost includes
-xcopy /Y /S "C:\Libraries\boost_1_63_0\boost" "C:\projects\fifengine-dependencies\includes\include\boost" >nul 2>&1
+xcopy /Y /S "C:\Libraries\boost_1_65_1\boost" "C:\projects\fifengine-dependencies\includes\include\boost" >nul 2>&1
 
 :: copy binaries (dlls) and libs
-copy "C:\Libraries\boost_1_63_0\lib32-msvc-14.0\boost_filesystem-vc140-mt-1_63.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_filesystem-vc140-mt-1_63.dll"
-copy "C:\Libraries\boost_1_63_0\lib32-msvc-14.0\boost_system-vc140-mt-1_63.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_system-vc140-mt-1_63.dll"
-copy "C:\Libraries\boost_1_63_0\lib32-msvc-14.0\boost_regex-vc140-mt-1_63.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_regex-vc140-mt-1_63.dll"
-copy "C:\Libraries\boost_1_63_0\lib32-msvc-14.0\boost_python-vc140-mt-1_63.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_regex-vc140-mt-1_63.dll"
-copy "C:\Libraries\boost_1_63_0\lib32-msvc-14.0\libboost_filesystem-vc140-mt-1_63.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_filesystem-vc140-mt-1_63.lib"
-copy "C:\Libraries\boost_1_63_0\lib32-msvc-14.0\libboost_system-vc140-mt-1_63.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_system-vc140-mt-1_63.lib"
-copy "C:\Libraries\boost_1_63_0\lib32-msvc-14.0\libboost_regex-vc140-mt-1_63.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_regex-vc140-mt-1_63.lib"
-copy "C:\Libraries\boost_1_63_0\lib32-msvc-14.0\libboost_python-vc140-mt-1_63.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_regex-vc140-mt-1_63.lib"
+copy "C:\Libraries\boost_1_65_1\lib32-msvc-14.1\boost_filesystem-vc141-mt-1_65.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_filesystem-vc141-mt-1_65.dll"
+copy "C:\Libraries\boost_1_65_1\lib32-msvc-14.1\boost_system-vc141-mt-1_65.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_system-vc141-mt-1_65.dll"
+copy "C:\Libraries\boost_1_65_1\lib32-msvc-14.1\boost_regex-vc141-mt-1_65.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_regex-vc141-mt-1_65.dll"
+copy "C:\Libraries\boost_1_65_1\lib32-msvc-14.1\boost_python-vc141-mt-1_65.dll" "C:\projects\fifengine-dependencies\includes\bin\boost_regex-vc141-mt-1_65.dll"
+copy "C:\Libraries\boost_1_65_1\lib32-msvc-14.1\libboost_filesystem-vc141-mt-1_65.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_filesystem-vc141-mt-1_65.lib"
+copy "C:\Libraries\boost_1_65_1\lib32-msvc-14.1\libboost_system-vc141-mt-1_65.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_system-vc141-mt-1_65.lib"
+copy "C:\Libraries\boost_1_65_1\lib32-msvc-14.1\libboost_regex-vc141-mt-1_65.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_regex-vc141-mt-1_65.lib"
+copy "C:\Libraries\boost_1_65_1\lib32-msvc-14.1\libboost_python-vc141-mt-1_65.lib" "C:\projects\fifengine-dependencies\includes\lib\boost\libboost_regex-vc141-mt-1_65.lib"
 
 :: remove the "share" folder
 rmdir /S /Q C:\projects\fifengine-dependencies\includes\share
