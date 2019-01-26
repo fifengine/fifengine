e876e86d09099e6f8a55d40c3db0ad4550c29c69
LinuxDonald <linuxdonald@posteo.de>
2019-01-26 15:57:55 +0100

    Update SDL2_ttf to version 2.0.15 Add more md5sum checksums into getsdl2_tff Update freetype in it to version 2.9.1 because it depends on that version now.

--- cmake/get-sdl2_ttf.cmake
+++ cmake/get-sdl2_ttf.cmake
@@ -5,7 +5,7 @@
 
 include(ExternalProject)
 
-set(SDL2_TTF_VERSION "2.0.14")
+set(SDL2_TTF_VERSION "2.0.15")
 
 # For win32 it's faster to download the pre-compiled development binaries.
 if (WIN32)
@@ -18,7 +18,7 @@ if (WIN32)
       PREFIX ${DEPENDENCY_EXTRACT_DIR}
       DOWNLOAD_DIR ${DEPENDENCY_DOWNLOAD_DIR}
       URL https://libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-${SDL2_TTF_VERSION}-mingw.tar.gz
-      URL_MD5 1
+      URL_MD5 9d537c47736ba8112ee63cc94871ff06
       CONFIGURE_COMMAND ""
       BUILD_COMMAND ""
       INSTALL_COMMAND 
@@ -36,7 +36,7 @@ if (WIN32)
       PREFIX ${DEPENDENCY_EXTRACT_DIR}
       DOWNLOAD_DIR ${DEPENDENCY_DOWNLOAD_DIR}
       URL https://libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-${SDL2_TTF_VERSION}-VC.zip
-      URL_MD5 304d4aa67fec7a5a8c3c47c4639b91e2
+      URL_MD5 26f762c8d9b011505529f1f73fc45f88
       CONFIGURE_COMMAND ""
       BUILD_COMMAND ""
       INSTALL_COMMAND 
@@ -51,8 +51,8 @@ else() # build from source
   ExternalProject_Add(
     freetype2
     PREFIX ${DEPENDENCY_EXTRACT_DIR}
-    URL http://download.savannah.gnu.org/releases/freetype/freetype-2.4.12.tar.gz
-    URL_MD5 1
+    URL http://download.savannah.gnu.org/releases/freetype/freetype-2.9.1.tar.gz
+    URL_MD5 3adb0e35d3c100c456357345ccfa8056
     DOWNLOAD_DIR ${DOWNLOAD_DIR}
     CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${DEPENDENCY_INSTALL_DIR}
   )
@@ -63,7 +63,7 @@ else() # build from source
     PREFIX ${DEPENDENCY_EXTRACT_DIR}
     DOWNLOAD_DIR ${DEPENDENCY_DOWNLOAD_DIR}
     URL https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-${SDL2_TTF_VERSION}.tar.gz 
-    URL_MD5 1
+    URL_MD5 04fe06ff7623d7bdcb704e82f5f88391
     CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${DEPENDENCY_INSTALL_DIR} --with-freetype-prefix=${DEPENDENCY_INSTALL_DIR}
   )
 
