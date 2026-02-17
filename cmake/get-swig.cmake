a4b37a2b54057140fa77b16175a3d6e51b448172
Jens A. Koch <jakoch@web.de>
2026-02-17 20:25:55 +0100

    windows build (swig): fix version again

--- cmake/get-swig.cmake
+++ cmake/get-swig.cmake
@@ -14,8 +14,6 @@ set(SWIG_VERSION "4.4.1")
 
 set(_SWIG_INSTALL_DIR ${DEPENDENCY_INSTALL_DIR}/../build-tools/swig)
 
-set(_SWIG_INSTALL_DIR ${DEPENDENCY_INSTALL_DIR}/../build-tools/swig)
-
 # For Windows it's faster to download the pre-compiled development binaries.
 if(WIN32)
   ExternalProject_Add(
@@ -23,9 +21,7 @@ if(WIN32)
     PREFIX ${DEPENDENCY_EXTRACT_DIR}
     DOWNLOAD_DIR ${DEPENDENCY_DOWNLOAD_DIR}
     URL https://downloads.sourceforge.net/project/swig/swigwin/swigwin-${SWIG_VERSION}/swigwin-${SWIG_VERSION}.zip
-    URL https://downloads.sourceforge.net/project/swig/swigwin/swigwin-${SWIG_VERSION}/swigwin-${SWIG_VERSION}.zip
-    URL_MD5 9402fd1f2a961cf2fe4afb7ff80dfa6f
-    INSTALL_DIR ${_SWIG_INSTALL_DIR}
+    URL_HASH MD5=9402fd1f2a961cf2fe4afb7ff80dfa6f
     INSTALL_DIR ${_SWIG_INSTALL_DIR}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
